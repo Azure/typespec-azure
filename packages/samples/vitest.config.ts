7dafdea0a462ff6e9694efbb6a18b53e8fc71b1e
Timothee Guerin <tiguerin@microsoft.com>
2023-12-27 15:47:00 -0800

    .

--- packages/samples/vitest.config.ts
+++ packages/samples/vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     name: "azure-samples",
     environment: "node",
-    testTimeout: 10000,
+    testTimeout: 30000,
     isolate: false,
     coverage: {
       reporter: ["cobertura", "json", "text"],
