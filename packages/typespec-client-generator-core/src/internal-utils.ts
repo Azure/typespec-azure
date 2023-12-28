7b3f0bc416967d24f4326a55ebdf3a069e466865
tadelesh <tadelesh.shi@live.cn>
2023-12-28 11:27:49 +0800

    fix regex code checking

--- packages/typespec-client-generator-core/src/internal-utils.ts
+++ packages/typespec-client-generator-core/src/internal-utils.ts
@@ -28,7 +28,7 @@ export function parseEmitterName(emitterName?: string): string {
   if (!emitterName) {
     throw new Error("No emitter name found in program");
   }
-  const regex = /.*(?:cadl|typespec)-([^\\/]*)/;
+  const regex = /(?:cadl|typespec)-([^\\/]*)/;
   const match = emitterName.match(regex);
   if (!match || match.length < 2) return "none";
   const language = match[1];
