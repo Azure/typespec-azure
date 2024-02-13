470348a50b7ce02ed4d7dfb8d00e13d355169dd5
Timothee Guerin <tiguerin@microsoft.com>
2024-02-12 16:15:56 -0800

    Simple hotfix

--- packages/typespec-azure-core/src/rules/no-enum.ts
+++ packages/typespec-azure-core/src/rules/no-enum.ts
@@ -1,4 +1,14 @@
-import { Enum, createRule, paramMessage } from "@typespec/compiler";
+import {
+  CodeFix,
+  Enum,
+  EnumMemberNode,
+  EnumSpreadMemberNode,
+  Node,
+  SyntaxKind,
+  createRule,
+  getSourceLocation,
+  paramMessage,
+} from "@typespec/compiler";
 import { getVersionsForEnum } from "@typespec/versioning";
 export const noEnumRule = createRule({
   name: "no-enum",
@@ -17,8 +27,65 @@ export const noEnumRule = createRule({
         context.reportDiagnostic({
           format: { enumName: en.name },
           target: en,
+          codefixes: [createEnumToExtensibleUnionCodeFix(en)],
         });
       },
     };
   },
 });
+
+function createEnumToExtensibleUnionCodeFix(en: Enum): CodeFix {
+  function convertEnumMemberToUnionVariant(node: EnumMemberNode | EnumSpreadMemberNode) {
+    switch (node.kind) {
+      case SyntaxKind.EnumSpreadMember:
+        return getSourceLocation(node.target).file.text.slice(node.target.pos, node.target.end);
+      case SyntaxKind.EnumMember:
+        return node.value
+          ? `${node.id.sv}: ${
+              node.value.kind === SyntaxKind.NumericLiteral
+                ? node.value.value
+                : `"${node.value.value}"`
+            }}`
+          : `"${node.id.sv}"`;
+    }
+  }
+
+  return {
+    id: "enum-to-extensible-union",
+    label: "Convert to extensible union",
+    fix(context) {
+      const type = typeof [...en.members.values()][0].value === "number" ? "number" : "string";
+      return [
+        context.replaceText(
+          getSourceLocation(en.node),
+          `${getNodeTrivia(en.node)}union ${en.name} {${type}, ${en.node.members.map(
+            (member) => `${getNodeTrivia(member)}${convertEnumMemberToUnionVariant(member)}`
+          )}}`
+        ),
+      ];
+    },
+  };
+}
+
+function getNodeTrivia(node: Node): string {
+  let endOfTrivia = node.pos;
+  for (const directive of node.directives ?? []) {
+    if (directive.pos > endOfTrivia) {
+      endOfTrivia = directive.pos;
+    }
+  }
+  for (const doc of node.docs ?? []) {
+    if (doc.pos > endOfTrivia) {
+      endOfTrivia = doc.pos;
+    }
+  }
+  if ("decorators" in node) {
+    for (const dec of node.decorators ?? []) {
+      if (dec.pos > endOfTrivia) {
+        endOfTrivia = dec.pos;
+      }
+    }
+  }
+
+  return getSourceLocation(node).file.text.slice(node.pos, endOfTrivia);
+}
