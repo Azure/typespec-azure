fd90846c7a4df8fe5e4629939596bc0a5ecdfc4c
tadelesh <tadelesh.shi@live.cn>
2024-02-23 17:16:33 +0800

    address review comment

--- packages/typespec-autorest/test/flatten.test.ts
+++ packages/typespec-autorest/test/flatten.test.ts
@@ -1,66 +1,28 @@
 import { deepStrictEqual } from "assert";
-import { describe, it } from "vitest";
+import { it } from "vitest";
 import { openApiFor } from "./test-host.js";
 
-describe("typespec-autorest: flatten", () => {
-  it("tcgc flatten decorator", async () => {
-    const res = await openApiFor(
-      `
-      @service
-      namespace Test;
+it("applies x-ms-client-flatten for property marked with @flattenProperty", async () => {
+  const res = await openApiFor(
+    `
+    model Widget {
+      #suppress "deprecated" "@flattenProperty decorator is not recommended to use."
+      @flattenProperty
+      properties?: WidgetProperties;
+    }
 
-      model Widget {
-        #suppress "deprecated" "@flattenProperty decorator is not recommended to use."
-        @flattenProperty
-        properties?: WidgetProperties;
-      }
-
-      model WidgetProperties {
-        prop: string;
-      }
-
-      op get(): void;
-      `
-    );
-    const model = res.definitions["Widget"]!;
-    deepStrictEqual(model, {
+    model WidgetProperties {
+    }
+    `
+  );
+  const model = res.definitions["Widget"]!;
+  deepStrictEqual(model, {
+    properties: {
       properties: {
-        properties: {
-          $ref: "#/definitions/WidgetProperties",
-          "x-ms-client-flatten": true,
-        },
+        $ref: "#/definitions/WidgetProperties",
+        "x-ms-client-flatten": true,
       },
-      type: "object",
-    });
-  });
-
-  it("openapi extension decorator", async () => {
-    const res = await openApiFor(
-      `
-      @service
-      namespace Test;
-
-      model Widget {
-        @extension("x-ms-client-flatten", true)
-        properties?: WidgetProperties;
-      }
-
-      model WidgetProperties {
-        prop: string;
-      }
-
-      op get(): void;
-      `
-    );
-    const model = res.definitions["Widget"]!;
-    deepStrictEqual(model, {
-      properties: {
-        properties: {
-          $ref: "#/definitions/WidgetProperties",
-          "x-ms-client-flatten": true,
-        },
-      },
-      type: "object",
-    });
+    },
+    type: "object",
   });
 });
