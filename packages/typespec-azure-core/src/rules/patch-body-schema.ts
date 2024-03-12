14a245091d373668b461ad525adbfe41cc5953ed
Travis Prescott <trpresco@microsoft.com>
2024-03-12 09:32:55 -0700

    Add rule and tests.

--- packages/typespec-azure-core/src/rules/patch-body-schema.ts
+++ packages/typespec-azure-core/src/rules/patch-body-schema.ts
@@ -1,54 +1,72 @@
-import { Operation, createRule, ignoreDiagnostics, paramMessage } from "@typespec/compiler";
-import { getHttpOperation } from "@typespec/http";
+import {
+  Model,
+  Operation,
+  createRule,
+  getVisibility,
+  ignoreDiagnostics,
+  paramMessage,
+} from "@typespec/compiler";
+import { Visibility, createMetadataInfo, getHttpOperation } from "@typespec/http";
 
 export const patchBodySchemaRule = createRule({
   name: "patch-body-schema-problem",
   description: "Warn about problematic PATCH bodies.",
   severity: "warning",
   messages: {
-    required: paramMessage`Operation '${"name"}' has a PATCH body with a required property. This is not recommended.`,
-    default: paramMessage`Operation '${"name"}' has a PATCH body with a default value. This is not recommended.`,
-    createVisibility: paramMessage`Operation '${"name"}' has a PATCH body with a "create" visibility. This is not recommended.`,
+    required: paramMessage`Operation '${"name"}' has a PATCH body parameter '${"param"}' that is required.`,
+    default: paramMessage`Operation '${"name"}' has a PATCH body parameter '${"param"}' that has a default value.`,
+    createVisibility: paramMessage`Operation '${"name"}' has a PATCH body parameter '${"param"}' that has a "create" visibility.`,
   },
   create(context) {
     return {
       operation: (op: Operation) => {
         const httpOp = ignoreDiagnostics(getHttpOperation(context.program, op));
-        //   const responses = httpOp.responses.flatMap((x) => x.responses);
-        //   const metadataInfo = createMetadataInfo(context.program, {
-        //     canonicalVisibility: Visibility.Read,
-        //   });
-
-        //   let firstResponse: Type | undefined = undefined;
-        //   for (const resp of responses) {
-        //     const body = resp.body;
-        //     if (body === undefined) continue;
-        //     const bodyType = body.type;
-
-        //     let currResponse: Type | undefined = undefined;
-        //     switch (bodyType.kind) {
-        //       case "Model":
-        //         const effModel = metadataInfo.getEffectivePayloadType(bodyType, Visibility.Read);
-        //         if (isErrorModel(context.program, effModel)) continue;
-        //         currResponse = effModel;
-        //         break;
-        //       default:
-        //         currResponse = bodyType;
-        //         break;
-        //     }
-        //     if (firstResponse === undefined) {
-        //       firstResponse = currResponse;
-        //     } else if (firstResponse !== currResponse) {
-        //       context.reportDiagnostic({
-        //         target: op,
-        //         messageId: "multipleSuccessSchemas",
-        //         format: {
-        //           name: op.name,
-        //         },
-        //       });
-        //       return;
-        //     }
-        //   }
+        if (httpOp.verb !== "patch") return;
+        const parameters = httpOp.parameters;
+        if (parameters.body === undefined) return;
+        const metadataInfo = createMetadataInfo(context.program, {
+          canonicalVisibility: Visibility.Read,
+        });
+        const effBody = metadataInfo.getEffectivePayloadType(
+          parameters.body.type as Model,
+          Visibility.Read
+        ) as Model;
+        for (const [name, prop] of effBody.properties) {
+          // Check for required properties
+          if (prop.optional === false) {
+            context.reportDiagnostic({
+              messageId: "required",
+              format: {
+                name: op.name,
+                param: name,
+              },
+              target: prop,
+            });
+          }
+          // Check for deafault values
+          if (prop.default !== undefined) {
+            context.reportDiagnostic({
+              messageId: "default",
+              format: {
+                name: op.name,
+                param: name,
+              },
+              target: prop,
+            });
+          }
+          // Check for "create-only" visibility
+          const visibility = getVisibility(context.program, prop);
+          if (visibility?.length === 1 && visibility[0] === "create") {
+            context.reportDiagnostic({
+              messageId: "createVisibility",
+              format: {
+                name: op.name,
+                param: name,
+              },
+              target: prop,
+            });
+          }
+        }
       },
     };
   },
--- packages/typespec-azure-core/test/rules/patch-body-schema.test.ts
+++ packages/typespec-azure-core/test/rules/patch-body-schema.test.ts
@@ -18,61 +18,69 @@ beforeEach(async () => {
 it("emits warning when PATCH body schema contains non-recommended patterns", async () => {
   await tester
     .expect(
-      `
-    @error
-    model Error {
-      code: int32;
-      message: string;
-    }
-
-    model Return200 {
-      @statusCode status: 200;
-      @body body: string;
-    }
-
-    model Return201 {
-      @statusCode status: 201;
-      @body body: int32;
-    }
-
-    @test op test(): Return200 | Return201 | Error;
-
-    `
+      `      
+      @resource("widgets")
+      model Widget {
+        @key("widgetName")
+        @visibility("read")
+        name: string;
+        color: string;
+        manufacturerId: string;
+        description: string;
+      }
+      
+      model WidgetPatch {
+        color: string;
+        manufacturerId?: string = "woo";
+        @visibility("create")
+        description?: string;
+      }
+      
+      interface Widgets {
+        @patch update is Azure.Core.Foundations.Operation<WidgetPatch, Widget>;
+      }`
     )
-    .toEmitDiagnostics({
-      code: "@azure-tools/typespec-azure-core/response-schema-problem",
-      severity: "warning",
-      message: `Operation 'test' has multiple non-error response schemas. Did you forget to add '@error' to one of them?`,
-    });
+    .toEmitDiagnostics([
+      {
+        code: "@azure-tools/typespec-azure-core/patch-body-schema-problem",
+        message: `Operation 'update' has a PATCH body parameter 'color' that is required.`,
+      },
+      {
+        code: "@azure-tools/typespec-azure-core/patch-body-schema-problem",
+        message: `Operation 'update' has a PATCH body parameter 'manufacturerId' that has a default value.`,
+      },
+      {
+        code: "@azure-tools/typespec-azure-core/patch-body-schema-problem",
+        message: `Operation 'update' has a PATCH body parameter 'description' that has a "create" visibility.`,
+      },
+    ]);
 });
 
-it("does not emit warning when PATCH body schema conforms to guidlines", async () => {
+it("does not emit warning when PATCH body schema conforms to guidelines", async () => {
   await tester
     .expect(
       `
-    @error
-    model Error {
-      code: int32;
-      message: string;
-    }
+      using Azure.Core.Traits;
 
-    model Return200 {
-      @statusCode status: 200;
-      name: string;
-    }
+      alias ServiceTraits = SupportsRepeatableRequests &
+        SupportsConditionalRequests &
+        SupportsClientRequestId;
 
-    model Return201 {
-      @statusCode status: 201;
-      age: int32;
-    }
-
-    @test op test(): Return200 | Return201 | Error;
-
-    `
+      alias Operations = Azure.Core.ResourceOperations<ServiceTraits>;
+      
+      @resource("widgets")
+      model Widget {
+        @key("widgetName")
+        @visibility("read")
+        name: string;
+        color: string;
+        manufacturerId: string;
+        description: string;
+      }
+      
+      interface Widgets {
+        update is Operations.ResourceUpdate<Widget>;
+      }`
     )
-    .toEmitDiagnostics({
-      code: "@azure-tools/typespec-azure-core/response-schema-problem",
-      severity: "warning",
-      message: `Operation 'test' has multiple non-error response schemas. Did you forget to add '@error' to one of them?`,
-    });
+    .toBeValid();
 });
