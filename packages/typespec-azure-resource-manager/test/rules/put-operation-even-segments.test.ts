5bc0dcd73370f7620cfdff8efd4c8e4dbc928b02
Travis Prescott <trpresco@microsoft.com>
2024-03-26 14:45:02 -0700

    Initial work.

--- packages/typespec-azure-resource-manager/test/rules/put-operation-even-segments.test.ts
+++ packages/typespec-azure-resource-manager/test/rules/put-operation-even-segments.test.ts
@@ -26,18 +26,30 @@ it("Emits a warning for put operations that don't have an even number of path se
       @armProviderNamespace
       namespace Microsoft.Contoso;
 
+      model Employee is ProxyResource<{}> {
+        @pattern("^[a-zA-Z0-9-]{3,24}$")
+        @key("employeeName")
+        @path
+        @segment("employees")
+        name: string;
+      }
+
+
       @armResourceOperations
       interface Operations {
-        @route("/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Music/Songs/{songName}")
-        operation1(@path subscriptionId: string, @path resourceGroupName: string, @path songName: string): void;  
+        @route("/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Employee")
+        @armResourceUpdate(Employee)
+        @put operation1(@path subscriptionId: string, @path resourceGroupName: string, @path employeeName: string): void;  
       }
-      // "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Music/Songs/{songName}/Artist/{artistName}"
-      // "/{resourceUri}/providers/Microsoft.Music/Songs/{songName}"
-      // "{resourceUri}/providers/Microsoft.Music/Songs/{songName}/Artist/{artistName}"
-      // "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Music/Songs/{songName}/providers/Microsoft.Album/Albums/{albumName}"
-      // "/providers/Microsoft.Music/Songs/{songName}"
-      // "/providers/Microsoft.Music/Songs/{songName}/Artist/{artistName}"
-      `
+      // "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Music"
+      // "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Music/Songs"
+      // "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Music/Songs/{songName}/Artist"
+      // "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Music/Songs/{songName}/Artist/addArtistName"
+      // "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Music/Songs/{songName}/Artist/{artistName}/addSong"
+      // "{resourceUri}/providers/Microsoft.Music/Songs/{songName}/Artist/{artistName}/addSong"
+      // "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Music/Songs/{songName}/providers/Microsoft.Album/Albums"
+      // "/providers/Microsoft.Music/Songs/{songName}/Artist/{artistName}/addSong"
+      // "/providers/Microsoft.Music/Songs"      `
     )
     .toEmitDiagnostics({
       code: "@azure-tools/typespec-azure-resource-manager/put-operation-even-segments",
@@ -61,18 +73,35 @@ it("Does not emit a warning for put operations that have an even number of path 
       }
       
       @armResourceOperations
-      interface Employees {
-        @put
-        @armResourceCreateOrUpdate(Employee)
-        createOrUpdate(...ApiVersionParameter): {
-          @statusCode _: 200;
-          result: boolean;
-        } | {
-          @statusCode _: 201;
-          result: boolean;
-        } | ErrorResponse;
-      }
-    `
+      interface Operations {
+        @route("/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Employee/Employees/{employeeName}")
+        @armResourceUpdate(Employee)
+        @put operation1(@path subscriptionId: string, @path resourceGroupName: string, @path employeeName: string): void;  
+        
+        @route("/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Employee/Employees/{employeeName}/Contact/{contactName}")
+        @armResourceUpdate(Employee)
+        @put operation2(@path subscriptionId: string, @path resourceGroupName: string, @path employeeName: string, @path contactName: string): void;
+
+        @route("/{resourceUri}/providers/Microsoft.Employee/Employees/{employeeName}")
+        @armResourceUpdate(Employee)
+        @put operation3(@path resourceUri: string, @path employeeName: string): void;
+
+        @route("/{resourceUri}/providers/Microsoft.Employee/Employees/{employeeName}/Contact/{contactName}")
+        @armResourceUpdate(Employee)
+        @put operation4(@path resourceUri: string, @path employeeName: string, @path contactName: string): void;
+
+        @route("/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Employee/Employees/{employeeName}/providers/Microsoft.Contact/Contacts/{contactName}")
+        @armResourceUpdate(Employee)
+        @put operation5(@path subscriptionId: string, @path resourceGroupName: string, @path employeeName: string, @path contactName: string): void;
+
+        @route("/providers/Microsoft.Employee/Employees/{employeeName}")
+        @armResourceUpdate(Employee)
+        @put operation6(@path employeeName: string): void;  
+
+        @route("/providers/Microsoft.Employee/Employees/{employeeName}/Contact/{contactName}")
+        @armResourceUpdate(Employee)
+        @put operation7(@path employeeName: string, @path contactName: string): void;
+      }`
     )
     .toBeValid();
 });
