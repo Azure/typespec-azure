0dd1e1e95ebc3122f076b0ceeb2a47c1603d6dfb
Allen Zhang <allenzhang@live.com>
2024-03-26 01:17:44 -0700

    Moving tests to sorting.test.ts

--- packages/typespec-autorest/test/sorting.test.ts
+++ packages/typespec-autorest/test/sorting.test.ts
@@ -1,8 +1,15 @@
-import { deepStrictEqual } from "assert";
+import { deepStrictEqual, strictEqual } from "assert";
 import { describe, it } from "vitest";
 import { sortOpenAPIDocument } from "../src/openapi.js";
+import { openApiFor } from "./test-host.js";
 
 describe("typespec-autorest: OpenAPI output should be determinstic", () => {
+  const headerDefinitions = `
+  model A_Header { @header a : string };
+  model B_Header { @header b : string };
+  model C_Header { @header c : string };
+`;
+
   it("sorts root", () => {
     const sorted = sortOpenAPIDocument({
       info: {} as any,
@@ -71,5 +78,35 @@ describe("typespec-autorest: OpenAPI output should be determinstic", () => {
       "options",
       "head",
     ]);
+  });
+
+  it("header already in lexical order", async () => {
+    const res = await openApiFor(
+      `
+      ${headerDefinitions}
+      model Headers { ...A_Header, ...B_Header, ...C_Header };
+
+      op read(): {@statusCode _: 200, content: string, headers: Headers};
+      `
+    );
+    strictEqual(Object.keys(res.paths["/"].get.responses["200"].headers).length, 3);
+    strictEqual(Object.keys(res.paths["/"].get.responses["200"].headers)[0], "a");
+    strictEqual(Object.keys(res.paths["/"].get.responses["200"].headers)[1], "b");
+    strictEqual(Object.keys(res.paths["/"].get.responses["200"].headers)[2], "c");
+  });
+
+  it("header not in lexical order", async () => {
+    const res = await openApiFor(
+      `
+      ${headerDefinitions}
+      model Headers { ...C_Header, ...A_Header, ...B_Header };
+
+      op read(): {@statusCode _: 200, content: string, headers: Headers};
+      `
+    );
+    strictEqual(Object.keys(res.paths["/"].get.responses["200"].headers).length, 3);
+    strictEqual(Object.keys(res.paths["/"].get.responses["200"].headers)[0], "a");
+    strictEqual(Object.keys(res.paths["/"].get.responses["200"].headers)[1], "b");
+    strictEqual(Object.keys(res.paths["/"].get.responses["200"].headers)[2], "c");
   });
 });
