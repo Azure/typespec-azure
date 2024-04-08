518de53590c7413fe53cb0bbc4407d53134df348
Timothee Guerin <timothee.guerin@outlook.com>
2024-04-08 10:26:02 -0700

    Migrate to eslint flat config (#621)
    resolve https://github.com/microsoft/typespec/issues/3121
    
    Core PR https://github.com/microsoft/typespec/pull/3123

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+// @ts-check
+import { dirname } from "path";
+import tsEslint from "typescript-eslint";
+import { fileURLToPath } from "url";
+import { TypeSpecCommonEslintConfigs, getTypeScriptProjectRules } from "./core/eslint.config.js";
+
+export default tsEslint.config(
+  {
+    ignores: [
+      "**/dist/**/*",
+      "**/.temp/**/*",
+      "**/generated-defs/*",
+      "**/website/build/**/*",
+      "**/.docusaurus/**/*",
+      "core/packages/compiler/templates/**/*", // Ignore the templates which might have invalid code and not follow exactly our rules.
+      // TODO: enable
+      "**/.scripts/**/*",
+      "eng/scripts/**/*",
+      "core/eng/scripts/**/*",
+      "packages/*/scripts/**/*",
+      "core/packages/*/scripts/**/*",
+    ],
+  },
+  ...TypeSpecCommonEslintConfigs,
+  ...getTypeScriptProjectRules(dirname(fileURLToPath(import.meta.url)))
+);
