ae0425107e2fc5b390507a9b49a07bae8724e922
Timothee Guerin <tiguerin@microsoft.com>
2024-04-09 08:31:36 -0700

    .

--- eslint.config.js
+++ eslint.config.js
@@ -16,6 +16,7 @@ export default tsEslint.config(
       // TODO: enable
       "**/.scripts/**/*",
       "eng/scripts/**/*",
+      "core/eng/common/scripts/**/*",
       "core/eng/tsp-core/scripts/**/*",
       "packages/*/scripts/**/*",
       "core/packages/*/scripts/**/*",
