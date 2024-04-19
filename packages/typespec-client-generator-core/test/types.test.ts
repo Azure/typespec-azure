62a9231bbad3687d4f6d6c6bc5497354723bb679
Chenjie Shi <tadelesh.shi@live.cn>
2024-04-20 05:24:07 +0800

    fix wrong judgement for array type (#692)
    @iscai-msft please help to review and release, thanks.
    
    fix: https://github.com/Azure/typespec-azure/issues/690
    
    cc: @haolingdong-msft

--- packages/typespec-client-generator-core/test/types.test.ts
+++ packages/typespec-client-generator-core/test/types.test.ts
@@ -3067,6 +3067,36 @@ describe("typespec-client-generator-core: types", () => {
       );
     });
   });
+
+  describe("SdkArrayType", () => {
+    it("use model is to represent array", async () => {
+      await runner.compile(`
+        @service({})
+        namespace TestClient {
+          model TestModel {
+            prop: string;
+          }
+          model TestArray is TestModel[];
+
+          op get(): TestArray;
+        }
+      `);
+      const models = runner.context.experimental_sdkPackage.models;
+      strictEqual(models.length, 1);
+      const model = models[0];
+      strictEqual(model.kind, "model");
+      strictEqual(model.name, "TestModel");
+      const client = runner.context.experimental_sdkPackage.clients[0];
+      ok(client);
+      const method = client.methods[0];
+      ok(method);
+      strictEqual(method.response.kind, "method");
+      strictEqual(method.response.type?.kind, "array");
+      strictEqual(method.response.type?.valueType.kind, "model");
+      strictEqual(method.response.type?.valueType.name, "TestModel");
+    });
+  });
+
   describe("SdkMultipartFormType", () => {
     it("multipart form basic", async function () {
       await runner.compileWithBuiltInService(`
