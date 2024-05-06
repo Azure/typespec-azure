6870e53c39bd38219a3bd9a02389d9949ca33423
tadelesh <tadelesh.shi@live.cn>
2024-05-06 15:15:44 +0800

    add option

--- packages/typespec-autorest/src/emit.ts
+++ packages/typespec-autorest/src/emit.ts
@@ -71,7 +71,7 @@ export async function $onEmit(context: EmitContext<AutorestEmitterOptions>) {
   const tracer = getTracer(context.program);
   tracer.trace("options", JSON.stringify(options, null, 2));
 
-  const tcgcSdkContext = createSdkContext(context, "@azure-tools/typespec-autorest");
+  const tcgcSdkContext = createSdkContext(context, "@azure-tools/typespec-autorest", { versionStrategy: "ignore" });
 
   await emitAllServiceAtAllVersions(context.program, tcgcSdkContext, options);
 }
