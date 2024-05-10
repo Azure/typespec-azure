d17367aaa47ab84204f4e7152df0a5bd1f6dec5d
Timothee Guerin <tiguerin@microsoft.com>
2024-05-10 13:16:37 -0700

    Fix

--- packages/typespec-client-generator-core/e2e/check-latest.e2e.ts
+++ packages/typespec-client-generator-core/e2e/check-latest.e2e.ts
@@ -1,5 +1,3 @@
-/* eslint-disable no-console */
-
 import { findTestPackageRoot } from "@typespec/compiler/testing";
 import { SpawnOptions, spawn } from "child_process";
 import { cp, readdir, rm } from "fs/promises";
@@ -13,7 +11,7 @@ let tgzFile: string;
 beforeAll(async () => {
   await rm(tempDir, { recursive: true, force: true });
 
-  await execAsync("pnpm", ["pack", "--pack-destination", tempDir]);
+  await execSuccessAsync("pnpm", ["pack", "--pack-destination", tempDir]);
   const files = await readdir(tempDir);
 
   const filename = files.find((x) => x.startsWith("azure-tools-typespec-client-generator-core-"));
@@ -28,8 +26,8 @@ beforeAll(async () => {
 // Make sure it works with the latest version of dependencies and not just the local build.
 it("works with latest version of packages", async () => {
   const dir = await setupScenario("basic-latest");
-  await execAsync("npm", ["install", tgzFile], { cwd: dir });
-  await execAsync("npx", ["tsp", "compile", ".", tgzFile], { cwd: dir });
+  await execSuccessAsync("npm", ["install", tgzFile], { cwd: dir });
+  await execSuccessAsync("npx", ["tsp", "compile", "."], { cwd: dir });
 });
 
 async function setupScenario(name: string): Promise<string> {
@@ -40,6 +38,16 @@ async function setupScenario(name: string): Promise<string> {
   return target;
 }
 
+async function execSuccessAsync(command: string, args: string[] = [], options: SpawnOptions = {}) {
+  const result = await execAsync(command, args, options);
+  if (result.exitCode !== 0) {
+    throw new Error(
+      `Command '${command} ${args.join(" ")}' failed with exit code ${result.exitCode}\n` +
+        result.stdio
+    );
+  }
+  return result;
+}
 async function execAsync(
   command: string,
   args: string[] = [],
