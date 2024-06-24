2876cb7d3d87ea7952683d5c3733a7ca33f39b55
Chenjie Shi <tadelesh.shi@live.cn>
2024-06-25 04:21:41 +0800

    export `SdkClientAccessor` (#990)
    fix: https://github.com/Azure/typespec-azure/issues/969

--- packages/typespec-client-generator-core/src/interfaces.ts
+++ packages/typespec-client-generator-core/src/interfaces.ts
@@ -532,7 +532,8 @@ export type SdkServiceMethod<TServiceOperation extends SdkServiceOperation> =
   | SdkLroServiceMethod<TServiceOperation>
   | SdkLroPagingServiceMethod<TServiceOperation>;
 
-interface SdkClientAccessor<TServiceOperation extends SdkServiceOperation> extends SdkMethodBase {
+export interface SdkClientAccessor<TServiceOperation extends SdkServiceOperation>
+  extends SdkMethodBase {
   kind: "clientaccessor";
   response: SdkClientType<TServiceOperation>;
 }
