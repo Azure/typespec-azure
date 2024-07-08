d51024aa4c4d2fc93a46890dc36defbd6c6cd602
iscai-msft <isabellavcai@gmail.com>
2024-07-08 17:49:05 -0400

    format

--- packages/typespec-client-generator-core/test/types/model-types.test.ts
+++ packages/typespec-client-generator-core/test/types/model-types.test.ts
@@ -1428,14 +1428,18 @@ describe("typespec-client-generator-core: model types", () => {
       `);
     const models = getAllModels(runner.context);
     strictEqual(models.length, 5);
-    const errorModels = models.filter((x) => x.kind === "model" && (x.usage & UsageFlags.Error) > 0);
+    const errorModels = models.filter(
+      (x) => x.kind === "model" && (x.usage & UsageFlags.Error) > 0
+    );
     deepStrictEqual(errorModels.map((x) => x.name).sort(), [
       "ApiError",
       "FiveHundredError",
       "FourHundredError",
       "FourZeroFourError",
     ]);
-    const validModel = models.filter((x) => x.kind === "model" && (x.usage & UsageFlags.Error) === 0);
+    const validModel = models.filter(
+      (x) => x.kind === "model" && (x.usage & UsageFlags.Error) === 0
+    );
     deepStrictEqual(
       validModel.map((x) => x.name),
       ["ValidResponse"]
