45a6c17c0849cab91034a7364fd5a232a91446f5
Haoling Dong <haolingdong@microsoft.com>
2024-07-09 17:35:31 +0800

    unit test

--- packages/typespec-client-generator-core/test/types/response.test.ts
+++ packages/typespec-client-generator-core/test/types/response.test.ts
@@ -26,4 +26,26 @@ describe("typespec-client-generator-core: responses", () => {
     strictEqual(responses[0].headers.length, 1);
     strictEqual(responses[0].headers[0].serializedName, "Content-Type");
   });
+
+  it("description shall be included in response", async () => {
+    await runner.compile(`
+      @service({})
+      namespace TestClient {
+        op get(): Test;
+
+        @doc("description on response")
+        model Test {
+          @body body: string;
+        }
+      }
+    `);
+    const client = runner.context.experimental_sdkPackage.clients[0];
+    ok(client);
+    const method = client.methods[0];
+    ok(method);
+    strictEqual(method.kind, "basic");
+    const responses = Array.from(method.operation.responses.values());
+    strictEqual(responses.length, 1);
+    strictEqual(responses[0].description, "description on response");
+  });
 });
