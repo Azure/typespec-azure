6058195b18e601e1d72afd5952640804d7620dd8
Timothee Guerin <timothee.guerin@outlook.com>
2024-07-18 13:51:55 -0700

    Arm library generated TS decorator signatures (#1200)

--- packages/typespec-azure-resource-manager/generated-defs/Azure.ResourceManager.ts
+++ packages/typespec-azure-resource-manager/generated-defs/Azure.ResourceManager.ts
@@ -1,13 +1,34 @@
-using TypeSpec.Reflection;
+import type {
+  DecoratorContext,
+  EnumMember,
+  Interface,
+  Model,
+  Namespace,
+  Operation,
+  Type,
+} from "@typespec/compiler";
 
-namespace Azure.ResourceManager;
+/**
+ * Marks the operation as being a collection action
+ */
+export type ArmResourceCollectionActionDecorator = (
+  context: DecoratorContext,
+  target: Operation
+) => void;
+
+/**
+ * `@armResourceType` sets the value fo the decorated string
+ * property to the type of the Azure Resource Manager resource.
+ *
+ * @param resource The resource to get the type of
+ */
+export type ArmProviderNameValueDecorator = (context: DecoratorContext, target: Operation) => void;
 
 /**
  * `@armProviderNamespace` sets the Azure Resource Manager provider name. It will default to use the
  * Namespace element value unless an override value is specified.
  *
  * @example
- *
  * ```typespec
  * @armProviderNamespace
  *  namespace Microsoft.Contoso;
@@ -17,138 +38,177 @@ namespace Azure.ResourceManager;
  * @armProviderNamespace("Microsoft.Contoso")
  *  namespace Microsoft.ContosoService;
  * ```
- *
  * @param providerNamespace Provider namespace
  * @param libraryNamespaces a library namespace containing types for this namespace
- *
  */
-extern dec armProviderNamespace(target: Namespace, providerNamespace?: valueof string);
+export type ArmProviderNamespaceDecorator = (
+  context: DecoratorContext,
+  target: Namespace,
+  providerNamespace?: string
+) => void;
 
 /**
  * Declare the Azure Resource Manager library namespaces used in this provider.
  * This allows sharing Azure Resource Manager resource types across specifications
+ *
  * @param namespaces The namespaces of Azure Resource Manager libraries used in this provider
  */
-extern dec useLibraryNamespace(target: Namespace, ...namespaces: Namespace[]);
+export type UseLibraryNamespaceDecorator = (
+  context: DecoratorContext,
+  target: Namespace,
+  ...namespaces: Namespace[]
+) => void;
 
 /**
  * `@armLibraryNamespace` designates a namespace as containign Azure Resource Manager Provider information.
  *
  * @example
- *
  * ```typespec
  * @armLibraryNamespace
  *  namespace Microsoft.Contoso;
  * ```
- *
  */
-extern dec armLibraryNamespace(target: Namespace);
+export type ArmLibraryNamespaceDecorator = (context: DecoratorContext, target: Namespace) => void;
 
 /**
  * `@singleton` marks an Azure Resource Manager resource model as a singleton resource.
  *
  * Singleton resources only have a single instance with a fixed key name.
- *  `.../providers/Microsoft.Contoso/monthlyReports/default`
+ * `.../providers/Microsoft.Contoso/monthlyReports/default`
  *
  * See more details on [different Azure Resource Manager resource type here.](https://azure.github.io/typespec-azure/docs/howtos/ARM/resource-type)
  *
  * @param keyValue The name of the singleton resource. Default name is "default".
  */
-extern dec singleton(target: Model, keyValue?: valueof string | "default");
+export type SingletonDecorator = (
+  context: DecoratorContext,
+  target: Model,
+  keyValue?: string | "default"
+) => void;
 
 /**
  * `@tenantResource` marks an Azure Resource Manager resource model as a Tenant resource/Root resource/Top-Level resource.
  *
  * Tenant resources have REST API paths like:
- *   `/provider/Microsoft.Contoso/FooResources`
+ * `/provider/Microsoft.Contoso/FooResources`
  *
  * See more details on [different Azure Resource Manager resource type here.](https://azure.github.io/typespec-azure/docs/howtos/ARM/resource-type)
  */
-extern dec tenantResource(target: Model);
+export type TenantResourceDecorator = (context: DecoratorContext, target: Model) => void;
 
 /**
  * `@subscriptionResource` marks an Azure Resource Manager resource model as a subscription resource.
  *
  * Subscription resources have REST API paths like:
- *  `/subscription/{id}/providers/Microsoft.Contoso/employees`
+ * `/subscription/{id}/providers/Microsoft.Contoso/employees`
  *
  * See more details on [different Azure Resource Manager resource type here.](https://azure.github.io/typespec-azure/docs/howtos/ARM/resource-type)
  */
-extern dec subscriptionResource(target: Model);
+export type SubscriptionResourceDecorator = (context: DecoratorContext, target: Model) => void;
 
 /**
  * `@locationResource` marks an Azure Resource Manager resource model as a location based resource.
  *
  * Location based resources have REST API paths like
- *   `/subscriptions/{subscriptionId}/locations/{location}/providers/Microsoft.Contoso/employees`
+ * `/subscriptions/{subscriptionId}/locations/{location}/providers/Microsoft.Contoso/employees`
  *
  * See more details on [different Azure Resource Manager resource type here.](https://azure.github.io/typespec-azure/docs/howtos/ARM/resource-type)
  */
-extern dec locationResource(target: Model);
+export type LocationResourceDecorator = (context: DecoratorContext, target: Model) => void;
 
 /**
  * `@resourceGroupResource` marks an Azure Resource Manager resource model as a resource group level resource.
  * This is the default option for Azure Resource Manager resources. It is provided for symmetry and clarity, and
  * you typically do not need to specify it.
  *
- *   `/subscription/{id}/resourcegroups/{rg}/providers/Microsoft.Contoso/employees`
+ * `/subscription/{id}/resourcegroups/{rg}/providers/Microsoft.Contoso/employees`
  *
  * See more details on [different Azure Resource Manager resource type here.](https://azure.github.io/typespec-azure/docs/howtos/ARM/resource-type)
  */
-extern dec resourceGroupResource(target: Model);
+export type ResourceGroupResourceDecorator = (context: DecoratorContext, target: Model) => void;
 
 /**
  * `@extensionResource` marks an Azure Resource Manager resource model as an Extension resource.
  * Extension resource extends other resource types. URL path is appended
  * to another segment {scope} which refers to another Resource URL.
  *
- *   `{resourceUri}/providers/Microsoft.Contoso/accessPermissions`
+ * `{resourceUri}/providers/Microsoft.Contoso/accessPermissions`
  *
  * See more details on [different Azure Resource Manager resource type here.](https://azure.github.io/typespec-azure/docs/howtos/ARM/resource-type)
  */
-extern dec extensionResource(target: Model);
+export type ExtensionResourceDecorator = (context: DecoratorContext, target: Model) => void;
 
 /**
- * `@armResourceType` sets the value fo the decorated string
- * property to the type of the Azure Resource Manager resource.
- * @param resource The resource to get the type of
- */
-extern dec armProviderNameValue(target: Operation);
-
-/**
- * Marks the operation as being a collection action
- */
-extern dec armResourceCollectionAction(target: Operation);
-
-/**
+ *
+ *
+ *
  * @param resourceType Resource model
  */
-extern dec armResourceAction(target: Operation, resourceType: Model);
+export type ArmResourceActionDecorator = (
+  context: DecoratorContext,
+  target: Operation,
+  resourceType: Model
+) => void;
 
 /**
+ *
+ *
+ *
  * @param resourceType Resource model
  */
-extern dec armResourceCreateOrUpdate(target: Operation, resourceType: Model);
+export type ArmResourceCreateOrUpdateDecorator = (
+  context: DecoratorContext,
+  target: Operation,
+  resourceType: Model
+) => void;
 
 /**
+ *
+ *
+ *
  * @param resourceType Resource model
  */
-extern dec armResourceRead(target: Operation, resourceType: Model);
+export type ArmResourceReadDecorator = (
+  context: DecoratorContext,
+  target: Operation,
+  resourceType: Model
+) => void;
 
 /**
+ *
+ *
+ *
  * @param resourceType Resource model
  */
-extern dec armResourceUpdate(target: Operation, resourceType: Model);
+export type ArmResourceUpdateDecorator = (
+  context: DecoratorContext,
+  target: Operation,
+  resourceType: Model
+) => void;
 
 /**
+ *
+ *
+ *
  * @param resourceType Resource model
  */
-extern dec armResourceDelete(target: Operation, resourceType: Model);
+export type ArmResourceDeleteDecorator = (
+  context: DecoratorContext,
+  target: Operation,
+  resourceType: Model
+) => void;
 
 /**
+ *
+ *
+ *
  * @param resourceType Resource model
  */
-extern dec armResourceList(target: Operation, resourceType: Model);
+export type ArmResourceListDecorator = (
+  context: DecoratorContext,
+  target: Operation,
+  resourceType: Model
+) => void;
 
 /**
  * This decorator is used to identify interfaces containing resource operations.
@@ -159,9 +219,14 @@ extern dec armResourceList(target: Operation, resourceType: Model);
  * It also adds a `@tag` decorator bearing the name of the interface so that all
  * of the operations will be grouped based on the interface name in generated
  * clients.
+ *
  * @param _ DEPRECATED
  */
-extern dec armResourceOperations(target: Interface, _?: unknown);
+export type ArmResourceOperationsDecorator = (
+  context: DecoratorContext,
+  target: Interface,
+  _?: Type
+) => void;
 
 /**
  * This decorator is used either on a namespace or a version enum value to indicate
@@ -169,25 +234,27 @@ extern dec armResourceOperations(target: Interface, _?: unknown);
  *
  * @param version The Azure.ResourceManager.CommonTypes.Versions for the desired common-types version or an equivalent string value like "v5".
  */
-extern dec armCommonTypesVersion(
+export type ArmCommonTypesVersionDecorator = (
+  context: DecoratorContext,
   target: Namespace | EnumMember,
-  version: valueof string | EnumMember
-);
+  version: string | unknown
+) => void;
 
 /**
  * This decorator is used on Azure Resource Manager resources that are not based on
  * Azure.ResourceManager common types.
  *
- * @param propertiesType: The type of the resource properties.
+ * @param propertiesType : The type of the resource properties.
  */
-extern dec armVirtualResource(target: Model);
+export type ArmVirtualResourceDecorator = (context: DecoratorContext, target: Model) => void;
 
 /**
  * This decorator sets the base type of the given resource.
  *
  * @param baseType The built-in parent of the resource, this can be "Tenant", "Subscription", "ResourceGroup", "Location", or "Extension"
  */
-extern dec resourceBaseType(
+export type ResourceBaseTypeDecorator = (
+  context: DecoratorContext,
   target: Model,
-  baseType: "Tenant" | "Subscription" | "ResourceGroup" | "Location" | "Extension"
-);
+  baseType: Type
+) => void;
