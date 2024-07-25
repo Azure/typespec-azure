01e5e0ddd699b2b6319f59dddc0bd1e58db6e433
Dapeng Zhang <dapzhang@microsoft.com>
2024-07-25 10:48:22 +0800

    Fix the duplicate usage flag value (#1231)
    The `Xml` in `UsageFlag` accidentally to have a duplicate value with an
    existing one, this PR fixes it.

--- packages/typespec-client-generator-core/src/interfaces.ts
+++ packages/typespec-client-generator-core/src/interfaces.ts
@@ -638,7 +638,7 @@ export enum UsageFlags {
   // Set when model is used in conjunction with an application/json content type.
   Json = 1 << 8,
   // Set when model is used in conjunction with an application/xml content type.
-  Xml = 1 << 8,
+  Xml = 1 << 9,
 }
 
 interface SdkExampleBase {
