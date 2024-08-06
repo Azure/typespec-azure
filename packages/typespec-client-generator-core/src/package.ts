4a5c0fb12342515613d4e18e9757009c8362f986
iscai-msft <43154838+iscai-msft@users.noreply.github.com>
2024-08-06 12:36:23 -0400

    [tcgc] make endpoint overridable (#1155)
    fixes #1153
    
    ---------
    
    Co-authored-by: iscai-msft <isabellavcai@gmail.com>

--- packages/typespec-client-generator-core/src/package.ts
+++ packages/typespec-client-generator-core/src/package.ts
@@ -1,14 +1,14 @@
 import { getLroMetadata, getPagedResult } from "@azure-tools/typespec-azure-core";
 import {
-  Diagnostic,
-  Operation,
-  Type,
   createDiagnosticCollector,
+  Diagnostic,
   getNamespaceFullName,
   getService,
   ignoreDiagnostics,
+  Operation,
+  Type,
 } from "@typespec/compiler";
-import { getServers } from "@typespec/http";
+import { getServers, HttpServer } from "@typespec/http";
 import { resolveVersions } from "@typespec/versioning";
 import { camelCase } from "change-case";
 import {
@@ -45,6 +45,7 @@ import {
   SdkServiceOperation,
   SdkServiceParameter,
   SdkType,
+  SdkUnionType,
   TCGCContext,
   UsageFlags,
 } from "./interfaces.js";
@@ -417,78 +418,113 @@ function getSdkMethods<TServiceOperation extends SdkServiceOperation>(
   return diagnostics.wrap(retval);
 }
 
+function getEndpointTypeFromSingleServer(
+  context: TCGCContext,
+  client: SdkClient | SdkOperationGroup,
+  server: HttpServer | undefined
+): [SdkEndpointType[], readonly Diagnostic[]] {
+  const diagnostics = createDiagnosticCollector();
+  const templateArguments: SdkPathParameter[] = [];
+  const defaultOverridableEndpointType: SdkEndpointType = {
+    kind: "endpoint",
+    serverUrl: "{endpoint}",
+    templateArguments: [
+      {
+        name: "endpoint",
+        isGeneratedName: true,
+        description: "Service host",
+        kind: "path",
+        onClient: true,
+        urlEncode: false,
+        optional: false,
+        serializedName: "endpoint",
+        correspondingMethodParams: [],
+        type: getTypeSpecBuiltInType(context, "string"),
+        isApiVersionParam: false,
+        apiVersions: context.__tspTypeToApiVersions.get(client.type)!,
+        crossLanguageDefinitionId: `${getCrossLanguageDefinitionId(context, client.service)}.endpoint`,
+        decorators: [],
+      },
+    ],
+    decorators: [],
+  };
+  const types: SdkEndpointType[] = [];
+  if (!server) return diagnostics.wrap([defaultOverridableEndpointType]);
+  for (const param of server.parameters.values()) {
+    const sdkParam = diagnostics.pipe(getSdkHttpParameter(context, param, undefined, "path"));
+    if (sdkParam.kind === "path") {
+      templateArguments.push(sdkParam);
+      sdkParam.onClient = true;
+      if (param.defaultValue && "value" in param.defaultValue) {
+        sdkParam.clientDefaultValue = param.defaultValue.value;
+      }
+      const apiVersionInfo = updateWithApiVersionInformation(context, param, client.type);
+      sdkParam.isApiVersionParam = apiVersionInfo.isApiVersionParam;
+      if (sdkParam.isApiVersionParam) {
+        sdkParam.clientDefaultValue = apiVersionInfo.clientDefaultValue;
+      }
+      sdkParam.apiVersions = getAvailableApiVersions(context, param, client.type);
+    } else {
+      diagnostics.add(
+        createDiagnostic({
+          code: "server-param-not-path",
+          target: param,
+          format: {
+            templateArgumentName: sdkParam.name,
+            templateArgumentType: sdkParam.kind,
+          },
+        })
+      );
+    }
+  }
+  const isOverridable =
+    templateArguments.length === 1 && server.url.startsWith("{") && server.url.endsWith("}");
+
+  if (templateArguments.length === 0) {
+    types.push(defaultOverridableEndpointType);
+    types[0].templateArguments[0].clientDefaultValue = server.url;
+  } else {
+    types.push({
+      kind: "endpoint",
+      serverUrl: server.url,
+      templateArguments,
+      decorators: [],
+    });
+    if (!isOverridable) {
+      types.push(defaultOverridableEndpointType);
+    }
+  }
+  return diagnostics.wrap(types);
+}
+
 function getSdkEndpointParameter(
   context: TCGCContext,
   client: SdkClient | SdkOperationGroup
 ): [SdkEndpointParameter, readonly Diagnostic[]] {
   const diagnostics = createDiagnosticCollector();
   const servers = getServers(context.program, client.service);
-  let type: SdkEndpointType;
-  let optional: boolean = false;
-  if (servers === undefined || servers.length > 1) {
-    // if there is no defined server url, or if there are more than one
-    // we will return a mandatory endpoint parameter in initialization
-    const name = "endpoint";
+  const types: SdkEndpointType[] = [];
+
+  if (servers === undefined) {
+    // if there is no defined server url, we will return an overridable endpoint
+    types.push(...diagnostics.pipe(getEndpointTypeFromSingleServer(context, client, undefined)));
+  } else {
+    for (const server of servers) {
+      types.push(...diagnostics.pipe(getEndpointTypeFromSingleServer(context, client, server)));
+    }
+  }
+  let type: SdkEndpointType | SdkUnionType;
+  if (types.length > 1) {
     type = {
-      kind: "endpoint",
-      serverUrl: "{endpoint}",
-      templateArguments: [
-        {
-          name,
-          isGeneratedName: true,
-          description: "Service host",
-          kind: "path",
-          onClient: true,
-          urlEncode: false,
-          optional: false,
-          serializedName: "endpoint",
-          correspondingMethodParams: [],
-          type: getTypeSpecBuiltInType(context, "string"),
-          isApiVersionParam: false,
-          apiVersions: context.__tspTypeToApiVersions.get(client.type)!,
-          crossLanguageDefinitionId: `${getCrossLanguageDefinitionId(context, client.service)}.endpoint`,
-          decorators: [],
-        },
-      ],
+      kind: "union",
+      values: types,
+      name: createGeneratedName(context, client.service, "Endpoint"),
+      isGeneratedName: true,
+      crossLanguageDefinitionId: getCrossLanguageDefinitionId(context, client.service),
       decorators: [],
     };
   } else {
-    // this means we have one server
-    const templateArguments: SdkPathParameter[] = [];
-    type = {
-      kind: "endpoint",
-      serverUrl: servers[0].url,
-      templateArguments,
-      decorators: [],
-    };
-    for (const param of servers[0].parameters.values()) {
-      const sdkParam = diagnostics.pipe(getSdkHttpParameter(context, param, undefined, "path"));
-      if (sdkParam.kind === "path") {
-        templateArguments.push(sdkParam);
-        sdkParam.onClient = true;
-        if (param.defaultValue && "value" in param.defaultValue) {
-          sdkParam.clientDefaultValue = param.defaultValue.value;
-        }
-        const apiVersionInfo = updateWithApiVersionInformation(context, param, client.type);
-        sdkParam.isApiVersionParam = apiVersionInfo.isApiVersionParam;
-        if (sdkParam.isApiVersionParam) {
-          sdkParam.clientDefaultValue = apiVersionInfo.clientDefaultValue;
-        }
-        sdkParam.apiVersions = getAvailableApiVersions(context, param, client.type);
-      } else {
-        diagnostics.add(
-          createDiagnostic({
-            code: "server-param-not-path",
-            target: param,
-            format: {
-              templateArgumentName: sdkParam.name,
-              templateArgumentType: sdkParam.kind,
-            },
-          })
-        );
-      }
-    }
-    optional = Boolean(servers[0].url.length && templateArguments.every((param) => param.optional));
+    type = types[0];
   }
   return diagnostics.wrap({
     kind: "endpoint",
@@ -499,7 +535,7 @@ function getSdkEndpointParameter(
     onClient: true,
     urlEncode: false,
     apiVersions: context.__tspTypeToApiVersions.get(client.type)!,
-    optional,
+    optional: false,
     isApiVersionParam: false,
     crossLanguageDefinitionId: `${getCrossLanguageDefinitionId(context, client.service)}.endpoint`,
     decorators: [],
