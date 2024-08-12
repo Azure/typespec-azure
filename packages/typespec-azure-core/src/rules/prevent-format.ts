c9b49e36e72afc0d8ea7988b6b191cda675125c6
Timothee Guerin <tiguerin@microsoft.com>
2024-08-12 14:45:19 -0700

    cleanup

--- packages/typespec-azure-core/src/rules/prevent-format.ts
+++ packages/typespec-azure-core/src/rules/prevent-format.ts
@@ -1,5 +1,4 @@
 import { ModelProperty, Scalar, createRule } from "@typespec/compiler";
-import { isExcludedCoreType } from "./utils.js";
 
 export const preventFormatUse = createRule({
   name: "no-format",
@@ -11,9 +10,6 @@ export const preventFormatUse = createRule({
   create(context) {
     return {
       scalar: (scalar: Scalar) => {
-        if (isExcludedCoreType(context.program, scalar)) {
-          return;
-        }
         for (const dec of scalar.decorators) {
           if (dec.decorator.name === "$format") {
             context.reportDiagnostic({
@@ -23,9 +19,6 @@ export const preventFormatUse = createRule({
         }
       },
       modelProperty: (model: ModelProperty) => {
-        if (isExcludedCoreType(context.program, model)) {
-          return;
-        }
         for (const dec of model.decorators) {
           if (dec.decorator.name === "$format") {
             context.reportDiagnostic({
