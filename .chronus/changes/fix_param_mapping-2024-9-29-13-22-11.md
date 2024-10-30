ba57ec54f198e411820b1fb4a0d3d6b96d55fb79
Chenjie Shi <tadelesh.shi@live.cn>
2024-10-31 04:13:28 +0800

    [tcgc] use root source property to map operation params to method (#1761)

---
+++ .chronus/changes/fix_param_mapping-2024-9-29-13-22-11.md
@@ -0,0 +1,7 @@
+---
+changeKind: fix
+packages:
+  - "@azure-tools/typespec-client-generator-core"
+---
+
+use root source property to map operation params to method
--- packages/typespec-client-generator-core/src/http.ts
+++ packages/typespec-client-generator-core/src/http.ts
@@ -595,7 +595,7 @@ function findMapping(
     if (
       methodParam.__raw &&
       serviceParam.__raw &&
-      methodParam.__raw.node === serviceParam.__raw.node
+      findRootSourceProperty(methodParam.__raw) === findRootSourceProperty(serviceParam.__raw)
     ) {
       return methodParam;
     }
@@ -626,6 +626,13 @@ function findMapping(
     }
   }
   return undefined;
+}
+
+function findRootSourceProperty(property: ModelProperty): ModelProperty {
+  while (property.sourceProperty) {
+    property = property.sourceProperty;
+  }
+  return property;
 }
 
 function getCollectionFormat(
