a24ce208d8f80129e8090c2fb16fbc8439a0c601
iscai-msft <isabellavcai@gmail.com>
2024-10-31 11:13:58 -0400

    format

--- packages/typespec-client-generator-core/src/http.ts
+++ packages/typespec-client-generator-core/src/http.ts
@@ -409,12 +409,12 @@ function getSdkHttpResponseAndExceptions(
   context: TCGCContext,
   httpOperation: HttpOperation,
 ): [
-    {
-      responses: SdkHttpResponse[];
-      exceptions: SdkHttpErrorResponse[];
-    },
-    readonly Diagnostic[],
-  ] {
+  {
+    responses: SdkHttpResponse[];
+    exceptions: SdkHttpErrorResponse[];
+  },
+  readonly Diagnostic[],
+] {
   const diagnostics = createDiagnosticCollector();
   const responses: SdkHttpResponse[] = [];
   const exceptions: SdkHttpErrorResponse[] = [];
