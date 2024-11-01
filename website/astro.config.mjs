7ddc8e9c03d812abf5dc93a5c7e46c1d5a8b067b
Timothee Guerin <tiguerin@microsoft.com>
2024-11-01 08:21:55 -0700

    sidebar

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,28 +1,38 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import starlight from "@astrojs/starlight";
+import { processSidebar } from "@typespec/astro-utils/sidebar";
+import astroExpressiveCode from "astro-expressive-code";
+import { defineConfig } from "astro/config";
+import { resolve } from "path";
+import current from "./src/content/current-sidebar";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    astroExpressiveCode(),
+    starlight({
+      title: "My Docs",
+      sidebar: await processSidebar(
+        resolve(import.meta.dirname, "src/content/docs"),
+        "docs",
+        current,
+      ),
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
