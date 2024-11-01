a5c430e99172b85bd1e3149846ee50809a0a9715
Timothee Guerin <tiguerin@microsoft.com>
2024-11-01 09:18:25 -0700

    progress

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
+import react from "@astrojs/react";
 import starlight from "@astrojs/starlight";
+import { TypeSpecLang } from "@typespec/astro-utils/shiki";
 import { processSidebar } from "@typespec/astro-utils/sidebar";
 import astroExpressiveCode from "astro-expressive-code";
+import rehypeAstroRelativeMarkdownLinks from "astro-rehype-relative-markdown-links";
 import { defineConfig } from "astro/config";
 import { resolve } from "path";
+import remarkHeadingID from "remark-heading-id";
 import current from "./src/content/current-sidebar";
 
 const base = process.env.TYPESPEC_WEBSITE_BASE_PATH ?? "/";
@@ -28,5 +32,19 @@ export default defineConfig({
         github: "https://github.com/Azure/typespec-azure",
       },
     }),
+    react(),
   ],
+  markdown: {
+    // @ts-expect-error wrong type
+    remarkPlugins: [remarkHeadingID],
+    rehypePlugins: [
+      [
+        rehypeAstroRelativeMarkdownLinks,
+        { base, contentPath: "src/content/docs", trailingSlash: "always" },
+      ],
+    ],
+    shikiConfig: {
+      langs: [TypeSpecLang],
+    },
+  },
 });
