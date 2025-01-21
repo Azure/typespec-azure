78b36d78370f5d098d0bb521dd4b3256ea8e6b28
Chenjie Shi <tadelesh.shi@live.cn>
2025-01-21 16:26:27 +0800

    Backmerge/release/january 2025 2025 01 21 (#2104)
    Co-authored-by: Weidong Xu <weidxu@microsoft.com>

--- packages/typespec-client-generator-core/src/package.ts
+++ packages/typespec-client-generator-core/src/package.ts
@@ -397,6 +397,8 @@ function getSdkMethodResponse(
   if (nonBodyExists && type) {
     type = {
       kind: "nullable",
+      name: createGeneratedName(context, operation, "NullableResponse"),
+      isGeneratedName: true,
       type: type,
       decorators: [],
       access: "public",
@@ -558,6 +560,7 @@ function getSdkInitializationType(
       clientNamespace: getClientNamespace(context, client.type),
       apiVersions: context.__tspTypeToApiVersions.get(client.type)!,
       decorators: [],
+      serializationOptions: {},
     };
   }
 
