74be3cba185422cb9a96964024f8151b526ac423
Timothee Guerin <timothee.guerin@outlook.com>
2025-01-25 12:59:38 -0800

    Add spector dashboard to website (#2130)

--- packages/azure-spec-dashboard/src/utils.tsx
+++
@@ -1,8 +0,0 @@
-import { DependencyList, useEffect } from "react";
-
-export function useEffectAsync(callback: () => Promise<void>, deps: DependencyList) {
-  return useEffect(() => {
-    void callback();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, deps);
-}
