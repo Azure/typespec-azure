7970d41114509e75ac46e712f715255d973ab3f0
iscai-msft <isabellavcai@gmail.com>
2025-03-04 12:43:43 -0500

    initial commit

--- packages/typespec-client-generator-core/src/http.ts
+++ packages/typespec-client-generator-core/src/http.ts
@@ -695,8 +695,9 @@ function getCollectionFormat(
   const program = context.program;
   if (isHeader(program, type)) {
     const headerOptions = getHeaderFieldOptions(program, type);
-
+    // eslint-disable-next-line @typescript-eslint/no-deprecated
     if (headerOptions.format) {
+      // eslint-disable-next-line @typescript-eslint/no-deprecated
       return headerOptions.format;
     } else if (typeof headerOptions.explode === "boolean" || $.array.is(type.type)) {
       return headerOptions.explode ? "multi" : "csv";
