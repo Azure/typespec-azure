27533c9c981eb01c21da291fee706e687fc4698a
iscai-msft <isabellavcai@gmail.com>
2025-03-05 12:38:19 -0500

    remove unused @header commit

--- packages/typespec-client-generator-core/test/packages/paged-operation.test.ts
+++ packages/typespec-client-generator-core/test/packages/paged-operation.test.ts
@@ -277,7 +277,7 @@ describe("typespec-client-generator-core: paged operation", () => {
         model ListTestResult {
           @pageItems
           items: Test[];
-          @continuationToken @header
+          @continuationToken
           nextToken?: string;
         }
         model Test {
@@ -300,8 +300,8 @@ describe("typespec-client-generator-core: paged operation", () => {
       strictEqual(method.pagingMetadata.continuationTokenResponseSegments?.length, 1);
       strictEqual(method.operation.responses[0].type?.kind, "model");
       strictEqual(
-        method.pagingMetadata.continuationTokenResponseSegments?.[0].__raw,
-        method.operation.responses[0].type.properties[1].__raw,
+        method.pagingMetadata.continuationTokenResponseSegments?.[0],
+        method.operation.responses[0].type.properties[1],
       );
     });
 
