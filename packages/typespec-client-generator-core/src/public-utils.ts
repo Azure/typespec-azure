ce2e2000d889c36c527691c03df70c19a6068885
iscai-msft <isabellavcai@gmail.com>
2025-03-06 12:05:33 -0500

    re-remove projected name tests

--- packages/typespec-client-generator-core/src/public-utils.ts
+++ packages/typespec-client-generator-core/src/public-utils.ts
@@ -13,7 +13,6 @@ import {
   getEffectiveModelType,
   getFriendlyName,
   getNamespaceFullName,
-  getProjectedName,
   ignoreDiagnostics,
   resolveEncodedName,
 } from "@typespec/compiler";
@@ -54,7 +53,6 @@ import {
   isHttpBodySpread,
   listAllServiceNamespaces,
   listAllUserDefinedNamespaces,
-  listAllServiceNamespaces,
   removeVersionsLargerThanExplicitlySpecified,
 } from "./internal-utils.js";
 import { createDiagnostic } from "./lib.js";
@@ -172,18 +170,11 @@ export function getPropertyNames(context: TCGCContext, property: ModelProperty):
 export function getLibraryName(
   context: TCGCContext,
   type: Type & { name?: string | symbol },
+  scope?: string | typeof AllScopes,
 ): string {
   // 1. check if there's a client name
-  let emitterSpecificName = getClientNameOverride(context, type);
+  const emitterSpecificName = getClientNameOverride(context, type, scope);
   if (emitterSpecificName && emitterSpecificName !== type.name) return emitterSpecificName;
-
-  // 2. check if there's a specific name for our language with deprecated @projectedName
-  emitterSpecificName = getProjectedName(context.program, type, context.emitterName);
-  if (emitterSpecificName && emitterSpecificName !== type.name) return emitterSpecificName;
-
-  // 3. check if there's a client name with deprecated @projectedName
-  const clientSpecificName = getProjectedName(context.program, type, "client");
-  if (clientSpecificName && emitterSpecificName !== type.name) return clientSpecificName;
 
   // 4. check if there's a friendly name, if so return friendly name
   const friendlyName = getFriendlyName(context.program, type);
@@ -221,11 +212,9 @@ export function getLibraryName(
  * @returns
  */
 export function getWireName(context: TCGCContext, type: Type & { name: string }) {
-  // 1. Check if there's an encoded name
   const encodedName = resolveEncodedName(context.program, type, "application/json");
   if (encodedName !== type.name) return encodedName;
-  // 2. Check if there's deprecated language projection
-  return getProjectedName(context.program, type, "json") ?? type.name;
+  return type.name;
 }
 
 /**
