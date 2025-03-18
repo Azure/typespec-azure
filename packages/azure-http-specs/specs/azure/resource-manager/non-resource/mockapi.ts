0b3ff4492e0748e0ca497be00f7e11608781483e
Pan Shao <pashao@microsoft.com>
2025-03-18 16:04:44 +0800

    Add non-resource arm tests

---
+++ packages/azure-http-specs/specs/azure/resource-manager/non-resource/mockapi.ts
@@ -0,0 +1,30 @@
+import { json, passOnSuccess, ScenarioMockApi } from "@typespec/spec-api";
+
+export const Scenarios: Record<string, ScenarioMockApi> = {};
+
+const SUBSCRIPTION_ID_EXPECTED = "00000000-0000-0000-0000-000000000000";
+const LOCATION_EXPECTED = "eastus";
+
+const nonResource = {
+  id: "id",
+  name: "hello",
+  type: "nonResource"
+};
+
+Scenarios.Azure_ResourceManager_NonResource_NonResourceOperations_get = passOnSuccess({
+  uri: "/subscriptions/:subscriptionId/providers/Microsoft.NonResource/locations/:location/otherParameters/:parameter",
+  method: "get",
+  request: {
+    params: {
+      subscriptionId: SUBSCRIPTION_ID_EXPECTED,
+      location: LOCATION_EXPECTED,
+      parameter: "hello",
+      "api-version": "2023-12-01-preview",
+    },
+  },
+  response: {
+    status: 200,
+    body: json(nonResource),
+  },
+  kind: "MockApiDefinition",
+});
