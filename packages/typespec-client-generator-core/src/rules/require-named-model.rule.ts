bb092cb536a0a31ca170a2a948b2354caac53d1c
iscai-msft <isabellavcai@gmail.com>
2025-04-01 12:39:41 -0400

    only validate if model is used in sdk generation

---
+++ packages/typespec-client-generator-core/src/rules/require-named-model.rule.ts
@@ -0,0 +1,34 @@
+import { createRule, Model, paramMessage } from "@typespec/compiler";
+import { createTCGCContext } from "../context.js";
+import { createSdkPackage } from "../package.js";
+
+export const requireNamedModelRule = createRule({
+  name: "require-named-model",
+  description: "Requires models to be named rather than defined anonymously or inline.",
+  severity: "warning",
+  url: "https://azure.github.io/typespec-azure/docs/libraries/typespec-client-generator-core/rules/require-named-model",
+  messages: {
+    default: paramMessage`Anonymous model detected. Define this model separately with a proper name to improve code readability and reusability.`,
+  },
+  create(context) {
+    const tcgcContext = createTCGCContext(
+      context.program,
+      "@azure-tools/typespec-client-generator-core",
+    );
+    // we create the package to see if the model is used in the final outpu
+    createSdkPackage(tcgcContext);
+    return {
+      model: (model: Model) => {
+        const createdModel = tcgcContext.__referencedTypeCache.get(model);
+        if (createdModel && createdModel.isGeneratedName) {
+          context.reportDiagnostic({
+            target: model,
+            format: {
+              generatedName: createdModel.name,
+            },
+          });
+        }
+      },
+    };
+  },
+});
---
+++ packages/typespec-client-generator-core/test/rules/require-named-model.test.ts
@@ -0,0 +1,134 @@
+import {
+  BasicTestRunner,
+  createLinterRuleTester,
+  LinterRuleTester,
+} from "@typespec/compiler/testing";
+import { beforeEach, it } from "vitest";
+import { requireNamedModelRule } from "../../src/rules/require-named-model.rule.js";
+import { createSdkTestRunner } from "../test-host.js";
+
+let runner: BasicTestRunner;
+let tester: LinterRuleTester;
+
+beforeEach(async () => {
+  runner = await createSdkTestRunner();
+  tester = createLinterRuleTester(
+    runner,
+    requireNamedModelRule,
+    "@azure-tools/typespec-client-generator-core",
+  );
+});
+
+it("anonymous model in property of another model", async () => {
+  await tester
+    .expect(
+      `
+      @service
+      namespace TestService {
+        @usage(Usage.input)
+        model NamedModel {
+          anonymousModelProp: {
+            foo: string;
+          };
+        }
+      }
+      
+      `,
+    )
+    .toEmitDiagnostics([
+      {
+        code: "@azure-tools/typespec-client-generator-core/require-named-model",
+        severity: "warning",
+        message: `Anonymous model detected. Define this model separately with a proper name to improve code readability and reusability.`,
+      },
+    ]);
+});
+
+it("nested anonymous model", async () => {
+  // should emit twice: once for each anonymous model
+  await tester
+    .expect(
+      `
+      @service
+      namespace TestService {
+        @usage(Usage.input)
+        model NamedModel {
+          nestedAnonymousModelProp: {
+            firstLevelProp: {
+              secondLevelProp: string;
+            }
+          };
+        }
+      }
+      `,
+    )
+    .toEmitDiagnostics([
+      {
+        code: "@azure-tools/typespec-client-generator-core/require-named-model",
+        severity: "warning",
+        message: `Anonymous model detected. Define this model separately with a proper name to improve code readability and reusability.`,
+      },
+      {
+        code: "@azure-tools/typespec-client-generator-core/require-named-model",
+        severity: "warning",
+        message: `Anonymous model detected. Define this model separately with a proper name to improve code readability and reusability.`,
+      },
+    ]);
+});
+
+it("anonymous model inline in operation request", async () => {
+  await tester
+    .expect(
+      `
+      @service
+      namespace TestService {
+        op foo(body: {prop: string}): void;
+      }
+      `,
+    )
+    .toEmitDiagnostics([
+      {
+        code: "@azure-tools/typespec-client-generator-core/require-named-model",
+        severity: "warning",
+        message: `Anonymous model detected. Define this model separately with a proper name to improve code readability and reusability.`,
+      },
+    ]);
+});
+
+it("anonymous model inline in operation response", async () => {
+  await tester
+    .expect(
+      `
+      @service
+      namespace TestService {
+        op foo(): {prop: string};
+      }
+      `,
+    )
+    .toEmitDiagnostics([
+      {
+        code: "@azure-tools/typespec-client-generator-core/require-named-model",
+        severity: "warning",
+        message: `Anonymous model detected. Define this model separately with a proper name to improve code readability and reusability.`,
+      },
+    ]);
+});
+
+it("anonymous model caused by spreading", async () => {
+  await tester
+    .expect(
+      `
+      @service
+      namespace TestService {
+        model Widget {
+          prop: string;
+        }
+        model OperationStatus<StatusResult> {
+          result?: StatusResult;
+        }
+        op foo(): AcceptedResponse & OperationStatus<Widget>;
+      }
+      `,
+    )
+    .toBeValid();
+});
