1d0c12308499e7e60c7a44970faed29a3bbf98e4
iscai-msft <isabellavcai@gmail.com>
2025-04-01 13:11:11 -0400

    add linter to rules package

--- packages/typespec-client-generator-core/src/linter.ts
+++ packages/typespec-client-generator-core/src/linter.ts
@@ -1,8 +1,9 @@
 import { defineLinter } from "@typespec/compiler";
 import { propertyNameConflictRule } from "./rules/property-name-conflict.rule.js";
 import { requireClientSuffixRule } from "./rules/require-client-suffix.rule.js";
+import { requireNamedModelRule } from "./rules/require-named-model.rule.js";
 
-const rules = [requireClientSuffixRule, propertyNameConflictRule];
+const rules = [requireClientSuffixRule, propertyNameConflictRule, requireNamedModelRule];
 
 const csharpRules = [propertyNameConflictRule];
 
