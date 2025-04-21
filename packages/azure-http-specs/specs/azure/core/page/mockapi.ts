39a7dbf741f1032fbd209a2ece04e6a119f16c9e
iscai-msft <isabellavcai@gmail.com>
2025-04-21 10:31:36 -0400

    add handler to access req.baseUrl

--- packages/azure-http-specs/specs/azure/core/page/mockapi.ts
+++ packages/azure-http-specs/specs/azure/core/page/mockapi.ts
@@ -1,4 +1,4 @@
-import { json, passOnSuccess, ScenarioMockApi } from "@typespec/spec-api";
+import { json, MockRequest, passOnSuccess, ScenarioMockApi } from "@typespec/spec-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 const validUser = { id: 1, name: "Madge", etag: "11bdc430-65e8-45ad-81d9-8ffa60d55b59" };
@@ -65,9 +65,17 @@ Scenarios.Azure_Core_Page_withParameterizedNextLink = passOnSuccess([
       body: json({
         value: [{ id: 1, name: "User1" }],
         // Include the original query parameters in the nextLink
-        nextLink:
-          "http://localhost:3000/azure/core/page/with-parameterized-next-link/second-page?select=name",
+        nextLink: `/azure/core/page/with-parameterized-next-link/second-page?select=name`,
       }),
+    },
+    handler: (req: MockRequest) => {
+      return {
+        status: 200,
+        body: json({
+          value: [{ id: 1, name: "User1" }],
+          nextLink: `${req.baseUrl}/azure/core/page/with-parameterized-next-link/second-page?select=${req.query.select}`,
+        }),
+      };
     },
     kind: "MockApiDefinition",
   },
