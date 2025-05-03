e4c4b640bb38554af9e0fcb2a36e06c3f57752c1
Timothee Guerin <timothee.guerin@outlook.com>
2025-05-02 17:15:50 -0700

    Uptake tspd alloy conversion (#2622)

--- packages/typespec-azure-resource-manager/generated-defs/Azure.ResourceManager.ts-test.ts
+++ packages/typespec-azure-resource-manager/generated-defs/Azure.ResourceManager.ts-test.ts
@@ -1,5 +1,10 @@
-/** An error here would mean that the decorator is not exported or doesn't have the right name. */
+// An error in the imports would mean that the decorator is not exported or
+// doesn't have the right name.
+
 import { $decorators } from "@azure-tools/typespec-azure-resource-manager";
 import type { AzureResourceManagerDecorators } from "./Azure.ResourceManager.js";
-/** An error here would mean that the exported decorator is not using the same signature. Make sure to have export const $decName: DecNameDecorator = (...) => ... */
+
+/**
+ * An error here would mean that the exported decorator is not using the same signature. Make sure to have export const $decName: DecNameDecorator = (...) => ...
+ */
 const _: AzureResourceManagerDecorators = $decorators["Azure.ResourceManager"];
