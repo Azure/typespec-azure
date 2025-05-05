c32ceef2c8ad407bfa6f51e984329c888a2643a8
AlitzelMendez <almend@microsoft.com>
2025-05-05 13:18:28 -0700

    is this the problem?

--- packages/typespec-azure-resource-manager/lib/common-types-generator/update-common-types.ts
+++ packages/typespec-azure-resource-manager/lib/common-types-generator/update-common-types.ts
@@ -6,15 +6,11 @@ import { generateCommonTypes } from "./gen.js";
 const args = process.argv.slice(2);
 
 if (args.includes("--update")) {
-  console.log("Updating common types...");
   const pathArgIndex = args.indexOf("--path");
   if (pathArgIndex !== -1 && args[pathArgIndex + 1]) {
     const destinationRoot = resolve(args[pathArgIndex + 1]);
     generateCommonTypes(destinationRoot);
   } else {
-    console.error(
-      "Missing --path argument. Usage: npx update-common-types --update --path <destination>",
-    );
     process.exit(1);
   }
 }
