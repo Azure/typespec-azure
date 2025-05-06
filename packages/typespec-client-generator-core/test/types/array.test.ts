432b53644489494bd45315ec146dfe12acdd9bbb
iscai-msft <isabellavcai@gmail.com>
2025-05-06 15:44:03 -0700

    add more tests

--- packages/typespec-client-generator-core/test/types/array.test.ts
+++ packages/typespec-client-generator-core/test/types/array.test.ts
@@ -109,6 +109,8 @@ it("same type's array come to same type", async () => {
         prop6: Test[][];
         prop7: Record<Test>[];
         prop8: Record<Test>[];
+        prop9: Record<Record<Test>>[];
+        prop10: Record<Record<Test>>[];
       }
 
       op get(): TestArray;
@@ -117,7 +119,7 @@ it("same type's array come to same type", async () => {
   const testArrayModel = runner.context.sdkPackage.models[0];
   strictEqual(testArrayModel.kind, "model");
   strictEqual(testArrayModel.name, "TestArray");
-  strictEqual(testArrayModel.properties.length, 8);
+  strictEqual(testArrayModel.properties.length, 10);
   const prop1 = testArrayModel.properties[0];
   const prop2 = testArrayModel.properties[1];
   const prop3 = testArrayModel.properties[2];
@@ -126,8 +128,11 @@ it("same type's array come to same type", async () => {
   const prop6 = testArrayModel.properties[5];
   const prop7 = testArrayModel.properties[6];
   const prop8 = testArrayModel.properties[7];
+  const prop9 = testArrayModel.properties[8];
+  const prop10 = testArrayModel.properties[9];
   strictEqual(prop1.type, prop2.type);
   strictEqual(prop3.type, prop4.type);
   strictEqual(prop5.type, prop6.type);
   strictEqual(prop7.type, prop8.type);
+  strictEqual(prop9.type, prop10.type);
 });
--- packages/typespec-client-generator-core/test/types/dictionary.test.ts
+++ packages/typespec-client-generator-core/test/types/dictionary.test.ts
@@ -23,6 +23,10 @@ it("same type's dictionary come to same type", async () => {
         prop4: Record<string>;
         prop5: Record<Test[]>;
         prop6: Record<Test[]>;
+        prop7: Record<Record<Test>>;
+        prop8: Record<Record<Test>>;
+        prop9: Record<Test[][]>;
+        prop10: Record<Test[][]>;
       }
       op get(): TestDictionary;
     }
@@ -30,14 +34,20 @@ it("same type's dictionary come to same type", async () => {
   const testArrayModel = runner.context.sdkPackage.models[0];
   strictEqual(testArrayModel.kind, "model");
   strictEqual(testArrayModel.name, "TestDictionary");
-  strictEqual(testArrayModel.properties.length, 6);
+  strictEqual(testArrayModel.properties.length, 10);
   const prop1 = testArrayModel.properties[0];
   const prop2 = testArrayModel.properties[1];
   const prop3 = testArrayModel.properties[2];
   const prop4 = testArrayModel.properties[3];
   const prop5 = testArrayModel.properties[4];
   const prop6 = testArrayModel.properties[5];
+  const prop7 = testArrayModel.properties[6];
+  const prop8 = testArrayModel.properties[7];
+  const prop9 = testArrayModel.properties[8];
+  const prop10 = testArrayModel.properties[9];
   strictEqual(prop1.type, prop2.type);
   strictEqual(prop3.type, prop4.type);
   strictEqual(prop5.type, prop6.type);
+  strictEqual(prop7.type, prop8.type);
+  strictEqual(prop9.type, prop10.type);
 });
