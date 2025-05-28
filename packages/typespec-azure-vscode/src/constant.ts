bc38b28d381086aafc2296840ba6ff06c334c35f
FAREAST\chunyu <chunyu@microsoft.com>
2025-05-28 17:06:19 +0800

    remove typespec extension installation and active

--- packages/typespec-azure-vscode/src/constant.ts
+++
@@ -1,3 +0,0 @@
-export const MainFileName = "main.tsp";
-export const ClientFileName = "client.tsp";
-export const TypespecExtensionId = "typespec.typespec-vscode";
--- packages/typespec-azure-vscode/src/extension.ts
+++ packages/typespec-azure-vscode/src/extension.ts
@@ -1,19 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import { TypespecExtensionId } from "./constant";
 
-export async function activate(context: vscode.ExtensionContext) {
-  // load and activate typespec extension
-  const typespecExtension = vscode.extensions.getExtension(TypespecExtensionId);
-  if (typespecExtension) {
-    await typespecExtension.activate();
-  } else {
-    vscode.window.showErrorMessage(
-      "Typespec extension is not installed. Please install it to use this extension.",
-    );
-  }
-}
+export async function activate(context: vscode.ExtensionContext) {}
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
