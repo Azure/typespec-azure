9b43d9790accde3fd8d4c49746121e1ed5685557
Timothee Guerin <tiguerin@microsoft.com>
2025-05-29 10:52:13 -0700

    regen

--- packages/typespec-azure-core/test/decorators.test.ts
+++ packages/typespec-azure-core/test/decorators.test.ts
@@ -13,7 +13,6 @@ import {
   getOperationLinks,
   getPagedResult,
   getParameterizedNextLinkArguments,
-  isFixed,
 } from "../src/decorators.js";
 import { FinalStateValue } from "../src/lro-helpers.js";
 import { createAzureCoreTestRunner } from "./test-host.js";
@@ -863,22 +862,6 @@ describe("typespec-azure-core: decorators", () => {
       `;
       const diagnostics = await runner.diagnose(code);
       expectDiagnosticEmpty(diagnostics);
-    });
-  });
-
-  describe("@fixed", () => {
-    it("marks `@fixed` enum correctly", async () => {
-      const result = await runner.compile(
-        `
-          @test @fixed enum FixedEnum {
-            A,
-            B,
-            C,
-          }
-          `,
-      );
-
-      ok(isFixed(runner.program, result.FixedEnum as Enum), "Expected fixed enum");
     });
   });
 
