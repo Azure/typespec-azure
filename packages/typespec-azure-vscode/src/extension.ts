358b6c459164d46fe5ec3860c748542bde13a870
FAREAST\chunyu <chunyu@microsoft.com>
2025-05-29 10:17:23 +0800

    resolve comments

--- packages/typespec-azure-vscode/src/extension.ts
+++ packages/typespec-azure-vscode/src/extension.ts
@@ -2,7 +2,9 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-export async function activate(context: vscode.ExtensionContext) {}
+export async function activate(context: vscode.ExtensionContext) {
+  vscode.window.showInformationMessage("Hello World from Typespec Azure VS Code!");
+}
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
