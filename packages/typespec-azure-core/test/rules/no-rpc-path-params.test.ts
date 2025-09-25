bb4c32c85115770d060592b921cbcc5df8018bf4
Timothee Guerin <timothee.guerin@outlook.com>
2025-09-25 13:59:33 -0700

    Migrate azure core Tester v2 (#3336)
    - Migrate Azure core to use Tester v2
    - Remove uncessary top level describe in modified files

--- packages/typespec-azure-core/test/rules/no-rpc-path-params.test.ts
+++ packages/typespec-azure-core/test/rules/no-rpc-path-params.test.ts
@@ -1,30 +1,20 @@
-import {
-  BasicTestRunner,
-  LinterRuleTester,
-  createLinterRuleTester,
-} from "@typespec/compiler/testing";
+import { Tester } from "#test/test-host.js";
+import { LinterRuleTester, createLinterRuleTester } from "@typespec/compiler/testing";
 import { beforeEach, describe, it } from "vitest";
 import { noRpcPathParamsRule } from "../../src/rules/no-rpc-path-params.js";
-import { createAzureCoreTestRunner } from "../test-host.js";
 
-describe("typespec-azure-core: RPC operations", () => {
-  let runner: BasicTestRunner;
-  let tester: LinterRuleTester;
+let tester: LinterRuleTester;
 
-  beforeEach(async () => {
-    runner = await createAzureCoreTestRunner();
-    tester = createLinterRuleTester(
-      runner,
-      noRpcPathParamsRule,
-      "@azure-tools/typespec-azure-core",
-    );
-  });
+beforeEach(async () => {
+  const runner = await Tester.createInstance();
+  tester = createLinterRuleTester(runner, noRpcPathParamsRule, "@azure-tools/typespec-azure-core");
+});
 
-  describe("cannot contain path parameters", () => {
-    it("as @path parameters", async () => {
-      await tester
-        .expect(
-          `
+describe("cannot contain path parameters", () => {
+  it("as @path parameters", async () => {
+    await tester
+      .expect(
+        `
           @route("/one")
           op testOne is RpcOperation<{ @path bar: string }, { }>;
 
@@ -33,44 +23,44 @@ describe("typespec-azure-core: RPC operations", () => {
           @route("/two")
           op testTwo is customOp<{}>;
         `,
-        )
-        .toEmitDiagnostics([
-          {
-            code: "@azure-tools/typespec-azure-core/no-rpc-path-params",
-            severity: "warning",
-            message: `Operations defined using RpcOperation should not have path parameters. Consider using ResourceAction or ResourceCollectionAction instead.`,
-          },
-          {
-            code: "@azure-tools/typespec-azure-core/no-rpc-path-params",
-            severity: "warning",
-            message: `Operations defined using RpcOperation should not have path parameters. Consider using ResourceAction or ResourceCollectionAction instead.`,
-          },
-        ]);
-    });
+      )
+      .toEmitDiagnostics([
+        {
+          code: "@azure-tools/typespec-azure-core/no-rpc-path-params",
+          severity: "warning",
+          message: `Operations defined using RpcOperation should not have path parameters. Consider using ResourceAction or ResourceCollectionAction instead.`,
+        },
+        {
+          code: "@azure-tools/typespec-azure-core/no-rpc-path-params",
+          severity: "warning",
+          message: `Operations defined using RpcOperation should not have path parameters. Consider using ResourceAction or ResourceCollectionAction instead.`,
+        },
+      ]);
+  });
 
-    it("as route parameters", async () => {
-      await tester
-        .expect(
-          `
+  it("as route parameters", async () => {
+    await tester
+      .expect(
+        `
           op customOp<TFoo extends TypeSpec.Reflection.Model> is RpcOperation<{ @path bar: string }, TFoo>;
 
           @route("/two/{bar}")
           op testTwo is customOp<{ }>;
         `,
-        )
-        .toEmitDiagnostics([
-          {
-            code: "@azure-tools/typespec-azure-core/no-rpc-path-params",
-            severity: "warning",
-            message: `Operations defined using RpcOperation should not have path parameters. Consider using ResourceAction or ResourceCollectionAction instead.`,
-          },
-        ]);
-    });
+      )
+      .toEmitDiagnostics([
+        {
+          code: "@azure-tools/typespec-azure-core/no-rpc-path-params",
+          severity: "warning",
+          message: `Operations defined using RpcOperation should not have path parameters. Consider using ResourceAction or ResourceCollectionAction instead.`,
+        },
+      ]);
+  });
 
-    it("also checks LongRunningRpcOperation", async () => {
-      await tester
-        .expect(
-          `
+  it("also checks LongRunningRpcOperation", async () => {
+    await tester
+      .expect(
+        `
           @doc("get lro status")
           @route("/lrRpcOp/{operationId}")
           @get op getStatus(@doc("The operation") @path operationId: string): PollingStatus;
@@ -85,16 +75,15 @@ describe("typespec-azure-core: RPC operations", () => {
           }
 
           model StatusError { message: string; }
-          @test @pollingOperation(getStatus) @route("/lrRpcOp") op lrRpcOp is Azure.Core.LongRunningRpcOperation<{ @path foo: string }, {}, PollingStatus, StatusError>;
+          @pollingOperation(getStatus) @route("/lrRpcOp") op lrRpcOp is Azure.Core.LongRunningRpcOperation<{ @path foo: string }, {}, PollingStatus, StatusError>;
         `,
-        )
-        .toEmitDiagnostics([
-          {
-            code: "@azure-tools/typespec-azure-core/no-rpc-path-params",
-            severity: "warning",
-            message: `Operations defined using RpcOperation should not have path parameters. Consider using ResourceAction or ResourceCollectionAction instead.`,
-          },
-        ]);
-    });
+      )
+      .toEmitDiagnostics([
+        {
+          code: "@azure-tools/typespec-azure-core/no-rpc-path-params",
+          severity: "warning",
+          message: `Operations defined using RpcOperation should not have path parameters. Consider using ResourceAction or ResourceCollectionAction instead.`,
+        },
+      ]);
   });
 });
