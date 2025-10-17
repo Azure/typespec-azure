1c180d3bb28f94651356cce795e794d306bc1977
Timothee Guerin <tiguerin@microsoft.com>
2025-10-17 15:35:05 -0700

    add new secret prop rule

---
+++ packages/typespec-azure-resource-manager/src/rules/secret-prop.ts
@@ -0,0 +1,45 @@
+import { ModelProperty, createRule, isSecret, paramMessage } from "@typespec/compiler";
+
+export const secretProprule = createRule({
+  name: "secret-prop",
+  description: `Check that property with names indicating sensitive information(e.g. contains auth, password, token, secret, etc.) are marked with @secret decorator.`,
+  severity: "warning",
+  url: "https://azure.github.io/typespec-azure/docs/libraries/azure-resource-manager/rules/secret-prop",
+  messages: {
+    default: paramMessage`Property '${"propertyName"}' looks like it contains sensitive information. Consider marking it with @secret decorator to ensure it is handled securely.`,
+  },
+  create(context) {
+    return {
+      modelProperty: (property: ModelProperty) => {
+        if (
+          isPotentialSensitiveProperty(property.name) &&
+          !isSecret(context.program, property) &&
+          !isSecret(context.program, property.type)
+        ) {
+          context.reportDiagnostic({
+            target: property,
+            format: {
+              propertyName: property.name,
+            },
+          });
+        }
+      },
+    };
+  },
+});
+
+const sensitiveKeywords = [
+  "access",
+  "credential",
+  "secret",
+  "password",
+  "key",
+  "token",
+  "auth",
+  "connection",
+].map((keyword) => keyword.toUpperCase());
+
+function isPotentialSensitiveProperty(propertyName: string): boolean {
+  const upperName = propertyName.toUpperCase();
+  return sensitiveKeywords.some((keyword) => upperName.includes(keyword));
+}
---
+++ packages/typespec-azure-resource-manager/test/rules/secret-prop.test.ts
@@ -0,0 +1,51 @@
+import { Tester } from "#test/tester.js";
+import { LinterRuleTester, createLinterRuleTester } from "@typespec/compiler/testing";
+import { beforeEach, describe, it } from "vitest";
+import { secretProprule } from "../../src/rules/secret-prop.js";
+
+let tester: LinterRuleTester;
+
+beforeEach(async () => {
+  tester = createLinterRuleTester(
+    await Tester.createInstance(),
+    secretProprule,
+    "@azure-tools/typespec-azure-resource-manager",
+  );
+});
+
+describe("emit warning for property containing words", () => {
+  it.each(["Auth", "Password", "Token", "Secret"])("for keyword %s", async (keyword) => {
+    await tester.expect(`model Test { some${keyword}Info: string; }`).toEmitDiagnostics({
+      code: "@azure-tools/typespec-azure-resource-manager/secret-prop",
+      message: `Property 'some${keyword}Info' looks like it contains sensitive information. Consider marking it with @secret decorator to ensure it is handled securely.`,
+    });
+  });
+});
+
+it("valid if the property has @secret", async () => {
+  await tester
+    .expect(
+      `
+        model Test {
+          @secret
+          somePasswordInfo: string;
+        }
+        `,
+    )
+    .toBeValid();
+});
+
+it("valid if the property type has @secret", async () => {
+  await tester
+    .expect(
+      `
+        model Test {
+          somePasswordInfo: myPassword;
+        }
+        
+        @secret
+        scalar myPassword extends string;
+        `,
+    )
+    .toBeValid();
+});
