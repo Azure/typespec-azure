0a5695775bcf9736983d2d2bde058efee0fac0bd
Mark Cowlishaw <markcowl@microsoft.com>
2025-11-05 19:44:12 -0800

    Fix #3477 Allow uniqueItems on nullable arrays

--- packages/typespec-azure-core/src/decorators.ts
+++ packages/typespec-azure-core/src/decorators.ts
@@ -1,6 +1,6 @@
 import { AzureCoreStateKeys } from "./lib.js";
 
-import { DecoratorContext, Model, ModelProperty, Program } from "@typespec/compiler";
+import { DecoratorContext, Model, ModelProperty, Program, Type } from "@typespec/compiler";
 import { getUniqueItems } from "./decorators/unique-items.js";
 
 export const $requestParameter = (context: DecoratorContext, entity: Model, name: string) => {
@@ -33,14 +33,14 @@ export function getResponseProperty(program: Program, entity: ModelProperty): st
  * @param type The model or model property to check.
  * @returns True if the model or model property has unique items, false otherwise.
  */
-export function hasUniqueItems(program: Program, type: Model | ModelProperty): boolean {
-  const cache = new WeakMap<Model | ModelProperty, boolean>();
+export function hasUniqueItems(program: Program, type: Type): boolean {
+  const cache = new WeakMap<Type, boolean>();
   return hasUniqueItemsCached(type);
-  function cacheResult(type: Model | ModelProperty, result: boolean): boolean {
+  function cacheResult(type: Type, result: boolean): boolean {
     cache.set(type, result);
     return result;
   }
-  function hasUniqueItemsCached(type: Model | ModelProperty): boolean {
+  function hasUniqueItemsCached(type: Type): boolean {
     if (cache.has(type)) {
       return cache.get(type)!;
     }
@@ -55,7 +55,7 @@ export function hasUniqueItems(program: Program, type: Model | ModelProperty): b
             type.sourceModels.some((sourceModel) => hasUniqueItemsCached(sourceModel.model))),
       );
     } else {
-      if (type.type.kind !== "Model") {
+      if (type.kind !== "ModelProperty") {
         return cacheResult(type, false);
       }
       return cacheResult(type, getUniqueItems(program, type) || hasUniqueItemsCached(type.type));
