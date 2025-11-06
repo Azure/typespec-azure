18770d511c54177d814160a9f3eb5fb24897593e
Mark Cowlishaw <1054056+markcowl@users.noreply.github.com>
2025-11-06 13:17:11 -0800

    Fix #3477 Allow uniqueItems on nullable arrays (#3481)

--- packages/typespec-azure-core/src/decorators/unique-items.ts
+++ packages/typespec-azure-core/src/decorators/unique-items.ts
@@ -1,4 +1,11 @@
-import { DecoratorContext, isArrayModelType, Model, ModelProperty, Type } from "@typespec/compiler";
+import {
+  DecoratorContext,
+  isArrayModelType,
+  isNullType,
+  Model,
+  ModelProperty,
+  Type,
+} from "@typespec/compiler";
 import { useStateMap } from "@typespec/compiler/utils";
 import type { UniqueItemsDecorator } from "../../generated-defs/Azure.Core.js";
 import { AzureCoreStateKeys, reportDiagnostic } from "../lib.js";
@@ -9,7 +16,9 @@ export const $uniqueItems: UniqueItemsDecorator = (
 ) => {
   const { program } = context;
   if (entity.kind === "ModelProperty") {
-    if (entity.type.kind !== "Model" || !isArrayModelType(program, entity.type)) {
+    if (
+      !isTypeOrNullableType(entity.type, (t) => t.kind === "Model" && isArrayModelType(program, t))
+    ) {
       reportDiagnostic(program, {
         code: "unique-items-invalid-type",
         target: entity,
@@ -32,3 +41,16 @@ export const $uniqueItems: UniqueItemsDecorator = (
 export const [getUniqueItems, setUniqueItems] = useStateMap<Type, boolean>(
   AzureCoreStateKeys.uniqueItems,
 );
+
+function isTypeOrNullableType(type: Type, filter: (type: Type) => boolean): boolean {
+  if (filter(type)) {
+    return true;
+  }
+  if (type.kind !== "Union") {
+    return false;
+  }
+  if (type.variants.size !== 2) return false;
+  const variants = [...type.variants.values()];
+  if (!filter(variants[0].type)) return false;
+  return isNullType(variants[1].type);
+}
