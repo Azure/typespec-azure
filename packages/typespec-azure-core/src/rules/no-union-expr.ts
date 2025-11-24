c1a3756dbed54636c3339f85f95d34b799694ac5
Timothee Guerin <tiguerin@microsoft.com>
2025-11-24 13:20:51 -0500

    better

--- packages/typespec-azure-core/src/rules/no-union-expr.ts
+++ packages/typespec-azure-core/src/rules/no-union-expr.ts
@@ -1,4 +1,4 @@
-import { createRule, paramMessage } from "@typespec/compiler";
+import { createRule, paramMessage, Program, Union } from "@typespec/compiler";
 import { $ } from "@typespec/compiler/typekit";
 import { getHeaderFieldName, isHeader, isStatusCode } from "@typespec/http";
 
@@ -12,26 +12,52 @@ export const noUnionExprRule = createRule({
   },
   create(context) {
     const program = context.program;
+    const excludedUnions = new Set<Union>();
+    const invalidUnions = new Set<Union>();
+
     return {
       modelProperty: (prop) => {
         const type = prop.type;
+        if (type.kind !== "Union" || type.name) {
+          return;
+        }
+
         if (
-          type.kind === "Union" &&
-          !type.name &&
-          !isStatusCode(program, prop) &&
-          !(
-            isHeader(program, prop) &&
-            getHeaderFieldName(program, prop).toLowerCase() === "content-type"
-          ) &&
-          [...type.variants.values()].filter((v) => v.type !== $(program).intrinsic.null).length > 1
+          isStatusCode(program, prop) ||
+          (isHeader(program, type) &&
+            getHeaderFieldName(program, type).toLowerCase() === "content-type")
         ) {
-          context.reportDiagnostic({
-            format: { enumName: type.name },
-            target: prop,
-            codefixes: [],
-          });
+          excludedUnions.add(type);
+        }
+      },
+      operation: (operation) => {
+        if (operation.returnType.kind === "Union") {
+          excludedUnions.add(operation.returnType);
+        }
+      },
+      union: (union) => {
+        if (union.kind === "Union" && !union.name && !isOnlyNullableUnion(program, union)) {
+          invalidUnions.add(union);
+        }
+      },
+      exit: () => {
+        for (const union of invalidUnions) {
+          if (!excludedUnions.has(union)) {
+            context.reportDiagnostic({
+              format: { enumName: union.name },
+              target: union,
+              codefixes: [],
+            });
+          }
         }
       },
     };
   },
 });
+
+/** Check if the union is only there to make the type nullable */
+function isOnlyNullableUnion(program: Program, union: Union): boolean {
+  return (
+    [...union.variants.values()].filter((v) => v.type !== $(program).intrinsic.null).length === 1
+  );
+}
