0715ad148eb74e24c3b284639dc9e220c5fbe0a8
Timothee Guerin <tiguerin@microsoft.com>
2025-12-15 14:54:30 -0500

    Generate x-ms-client-default

--- packages/typespec-autorest/src/openapi2-document.ts
+++ packages/typespec-autorest/src/openapi2-document.ts
@@ -112,7 +112,14 @@ export type JsonType = "array" | "boolean" | "integer" | "number" | "object" | "
 export type OpenAPI2SchemaRefProperty = Ref<OpenAPI2Schema> &
   Pick<
     OpenAPI2Schema,
-    "readOnly" | "description" | "default" | "x-ms-mutability" | "title" | "xml" | "externalDocs"
+    | "readOnly"
+    | "description"
+    | "default"
+    | "x-ms-mutability"
+    | "title"
+    | "xml"
+    | "externalDocs"
+    | "x-ms-client-default"
   > & {
     /**
      * Provide a different name to be used in the client.
@@ -286,6 +293,9 @@ export type OpenAPI2Schema = Extensions & {
   minProperties?: number;
 
   "x-ms-mutability"?: string[];
+
+  /** Default value set by the client */
+  "x-ms-client-default"?: unknown;
 
   /**
    * XML metadata for the schema, if applicable.
