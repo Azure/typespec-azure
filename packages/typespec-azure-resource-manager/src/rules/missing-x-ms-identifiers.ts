0d3cc19b86fcb8d546b3725b2ff05d2ee0ed4be2
Qiaoqiao Zhang <qiaozha@microsoft.com>
2026-01-15 15:53:36 +0800

    Fix build errors and disable x-ms-identifiers rule
    - Remove identifiers.test.ts that references deleted functions
    - Replace @identifiers with @OpenAPI.extension in library files
    - Disable missing-x-ms-identifiers rule (mark as deprecated)
    - Update rule message to indicate deprecation

--- packages/typespec-azure-resource-manager/src/rules/missing-x-ms-identifiers.ts
+++ packages/typespec-azure-resource-manager/src/rules/missing-x-ms-identifiers.ts
@@ -1,14 +1,8 @@
 import {
-  ArrayModelType,
   ModelProperty,
-  Program,
   createRule,
-  getProperty,
-  isArrayModelType,
   paramMessage,
 } from "@typespec/compiler";
-import { getExtensions } from "@typespec/openapi";
-import { isArmCommonType } from "../common-types.js";
 
 export const missingXmsIdentifiersRule = createRule({
   name: "missing-x-ms-identifiers",
@@ -16,78 +10,16 @@ export const missingXmsIdentifiersRule = createRule({
   severity: "warning",
   url: "https://azure.github.io/typespec-azure/docs/libraries/azure-resource-manager/rules/missing-x-ms-identifiers",
   messages: {
-    default: `Missing x-ms-identifiers. Decorate the property with @OpenAPI.extension("x-ms-identifiers", #["<identifying-property-name>"]). If there are no appropriate identifying properties, please add @OpenAPI.extension("x-ms-identifiers", #[]).`,
+    default: `Array property is missing x-ms-identifiers. This extension has been deprecated. Only add if required by legacy OpenAPI consumers.`,
     notArray: paramMessage`Value passed to @OpenAPI.extension("x-ms-identifiers", ...) was a "${"valueType"}". Pass an array of property name.`,
     missingProperty: paramMessage`Property "${"propertyName"}" is not found in "${"targetModelName"}". Make sure value of x-ms-identifiers extension are valid property name of the array element.`,
   },
   create(context) {
     return {
       modelProperty: (property: ModelProperty) => {
-        const type = property.type;
-        if (type.kind === "Model" && isArrayModelType(context.program, type)) {
-          if (isArrayMissingIdentifier(context.program, type, property)) {
-            context.reportDiagnostic({
-              target: property,
-            });
-          }
-        }
+        // Rule disabled - x-ms-identifiers is deprecated
+        return;
       },
     };
-
-    function isArrayMissingIdentifier(
-      program: Program,
-      array: ArrayModelType,
-      property: ModelProperty,
-    ) {
-      const elementType = array.indexer.value;
-      if (elementType.kind !== "Model") {
-        return false;
-      }
-
-      if (isArmCommonType(elementType)) {
-        return false;
-      }
-
-      if (getProperty(elementType, "id")) {
-        return false;
-      }
-
-      const xmsIdentifiers = getExtensions(program, property ?? array).get("x-ms-identifiers");
-      if (xmsIdentifiers === undefined) {
-        return true;
-      }
-
-      if (Array.isArray(xmsIdentifiers)) {
-        for (const propIdentifier of xmsIdentifiers) {
-          if (typeof propIdentifier === "string") {
-            const props = propIdentifier.replace(/^\//, "").split("/");
-            let element = elementType;
-            for (const prop of props) {
-              const propertyValue = getProperty(element, prop);
-              if (propertyValue === undefined) {
-                context.reportDiagnostic({
-                  messageId: "missingProperty",
-                  format: { propertyName: prop, targetModelName: elementType.name },
-                  target: property,
-                });
-              }
-
-              const propertyType = propertyValue?.type as ArrayModelType;
-              if (propertyType !== undefined && propertyType.kind === "Model") {
-                element = propertyType;
-              }
-            }
-          } else {
-            context.reportDiagnostic({
-              messageId: "notArray",
-              format: { valueType: typeof propIdentifier },
-              target: property,
-            });
-          }
-        }
-      }
-
-      return false;
-    }
   },
 });
