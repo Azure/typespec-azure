54cee0b4c4e09dfe618dd4ca47032b5a99e8223a
Joel Hendrix <jhendrix@microsoft.com>
2026-08-18 19:58:10 +0000

    Add coverage for new ARM scenarios (#5255)

---
+++ packages/typespec-go/test/azure-http-specs/azure/resource-manager/templatesgroup/paging_client_test.go
@@ -0,0 +1,53 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package templatesgroup_test
+
+import (
+	"context"
+	"templatesgroup"
+	"testing"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPagingClient_NewMarkAsPageablePager(t *testing.T) {
+	client := clientFactory.NewPagingClient()
+	require.NotNil(t, client)
+	pager := client.NewMarkAsPageablePager(resourceGroupExpected, "monitor1", nil)
+	require.NotNil(t, pager)
+
+	require.True(t, pager.More())
+	page, err := pager.NextPage(context.Background())
+	require.NoError(t, err)
+	require.Len(t, page.Value, 2)
+
+	require.Equal(t, "collection1", *page.Value[0].Name)
+	require.Equal(t, "Test Collection", *page.Value[0].Properties.DisplayName)
+	require.Equal(t, "collection2", *page.Value[1].Name)
+	require.Equal(t, "Another Collection", *page.Value[1].Properties.DisplayName)
+
+	require.False(t, pager.More())
+}
+
+func TestPagingClient_NewPostActionPagingPager(t *testing.T) {
+	client := clientFactory.NewPagingClient()
+	require.NotNil(t, client)
+	pager := client.NewPostActionPagingPager(resourceGroupExpected, "monitor1", &templatesgroup.PagingClientPostActionPagingOptions{
+		Body: &templatesgroup.LogStatusRequest{
+			Filter: to.Ptr("status eq 'active'"),
+		},
+	})
+	require.NotNil(t, pager)
+
+	require.True(t, pager.More())
+	page, err := pager.NextPage(context.Background())
+	require.NoError(t, err)
+	require.Len(t, page.Value, 1)
+	require.NotNil(t, page.Value[0].ID)
+	require.NotNil(t, page.Value[0].SendingMetrics)
+	require.True(t, *page.Value[0].SendingMetrics)
+
+	require.False(t, pager.More())
+}
