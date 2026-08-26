3af10d6c4bd9eef54b06c56e3ea8a7aaf23981fb
Chenjie Shi <tadelesh.shi@live.cn>
2026-08-26 14:52:53 +0800

    Remove headers from mock API request

--- packages/azure-http-specs/specs/azure/client-generator-core/override/mockapi.ts
+++ packages/azure-http-specs/specs/azure/client-generator-core/override/mockapi.ts
@@ -76,11 +76,7 @@ Scenarios.Azure_ClientGenerator_Core_Override_AddParameter_add = passOnSuccess([
   {
     uri: "/azure/client-generator-core/override/add",
     method: "get",
-    request: {
-      headers: {
-        "trace-id": "trace-id",
-      },
-    },
+    request: {},
     response: {
       status: 204,
     },
